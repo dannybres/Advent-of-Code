8217b961a01559eedd76603c82545db0d012d6c5
Daniel Breslan <daniel@breslan.co.uk>
2022-12-05 10:24:18 +0000

    Added comments for the “muggles”

--- 2022/Day 05/day5puzzle2.m
+++ 2022/Day 05/day5puzzle2.m
@@ -1,23 +1,31 @@
 %% day5puzzle2 - Daniel Breslan - Advent Of Code 2022
 data = readlines("input.txt");
 
-numberIdx = find(data.contains("  ") & ~data.contains("["));
+numberIdx = find(data.contains("  ") & ~data.contains("[")); % find line 
+% with " 1   2   3 ..."
 
-positions = char(data(1:numberIdx - 1));
+positions = char(data(1:numberIdx - 1)); % get the positions of the creates 
+% before numbers
 instructions = data(numberIdx + 2:end).erase(["move ","from ","to "])...
-    .split(" ").double();
+    .split(" ").double(); % get the instructions (after numbers) and format 
+% into n-x-3 array
 
-positions = string(rot90(positions(:,2:4:end),3)).erase(" ");
+positions = string(rot90(positions(:,2:4:end),3)).erase(" "); %turns the 
+% positsions into strings rotated clock wise so stack 1 is first index etc.
 
-for idx = 1:size(instructions,1)
+for idx = 1:size(instructions,1) % loop through instuctions
     stringToMove = positions(instructions(idx,2))...
         .extractAfter(strlength(positions(instructions(idx,2))) - ...
-        instructions(idx,1));
+        instructions(idx,1)); % find crates to move
     positions(instructions(idx,2)) = positions(instructions(idx,2))...
         .extractBefore(strlength(positions(instructions(idx,2))) - ...
-        instructions(idx,1)+1);
+        instructions(idx,1)+1); % remove crates from stack (remove from 
+    % string)
     positions(instructions(idx,3)) = positions(instructions(idx,3))...
-        .append(stringToMove);
+        .append(stringToMove); % add crates to other stack (append to 
+    % string)
 end
 
-day5puzzle2result = positions.extractAfter(strlength(positions)-1).join("")
+day5puzzle2result = positions.extractAfter(strlength(positions)-1)...
+    .join("") % get all the last elements of the stacks.
+
