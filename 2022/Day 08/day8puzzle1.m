4267682895904d1c58dcc8032904e65864b5e513
Daniel Breslan <daniel@breslan.co.uk>
2022-12-08 09:01:09 +0000

    Eww, nested foor loops.

--- 2022/Day 08/day8puzzle1.m
+++ 2022/Day 08/day8puzzle1.m
@@ -1,5 +1,18 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split("").double;
 
+data = data(:,2:end-1);
 
-day8puzzle1result = 0;
+isVisible = true(size(data));
+
+for r = 2:size(data,1)-1
+    for c = 2:size(data,2)-1
+        isVisible(r,c) =...
+            all(data(r,1:c-1) < data(r,c)) |...
+            all(data(r,c+1:end) < data(r,c)) |...
+            all(data(1:r-1,c) < data(r,c)) |...
+            all(data(r+1:end,c) < data(r,c));
+    end
+end
+
+day8puzzle1result = sum(isVisible,'all') %#ok<NOPTS> 
--- 2022/Day 08/day8puzzle2.m
+++ 2022/Day 08/day8puzzle2.m
@@ -1,5 +1,27 @@
 %% day8puzzle2 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split("").double;
 
+data = data(:,2:end-1);
 
-day8puzzle2result = 0;
+scenicScore = zeros(size(data));
+
+for r = 2:size(data,1)-1
+    for c = 3:size(data,2)-1
+        up = processView(data(r-1:-1:1,c)' >= data(r,c));
+        down = processView(data(r+1:end,c)' >= data(r,c));
+        left = processView(data(r,c-1:-1:1) >= data(r,c));
+        right = processView(data(r,c+1:end) >= data(r,c));
+        
+        scenicScore(r,c) = up * down * left * right;
+    end
+end
+
+day8puzzle2result = max(scenicScore(:)) %#ok<NOPTS> 
+
+function out = processView(in)
+    if sum(in) == 0
+        out = numel(in);
+        return
+    end
+    out = find(in,1);
+end
