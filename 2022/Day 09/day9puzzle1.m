0d6644acf0c40f50a857477453309b8bee9730fc
Daniel Breslan <daniel@breslan.co.uk>
2022-12-09 10:48:32 +0000

    comments

--- 2022/Day 09/day9puzzle1.m
+++ 2022/Day 09/day9puzzle1.m
@@ -1,21 +1,22 @@
 %% day9puzzle1 - Daniel Breslan - Advent Of Code 2022
 data = readlines("input.txt").replace(["U","D","L","R"],string(1:4))...
-    .split(" ").double();
+    .split(" ").double(); % load with UDLR as 1 2 3 4 m-x-2 matrix
 
 dirs = [...
-    0  1;...   % U 
+    0  1;...   % U - Define direction moves
     0  -1;...  % D
     -1 0;...   % L
     1  0       % R
     ];
 
-hlocations = [0 0;cumsum(dirs(repelem(data(:,1),data(:,2),1),:))];
-tlocations = zeros(size(hlocations));
+hlocations = [0 0;cumsum(dirs(repelem(data(:,1),data(:,2),1),:))]; 
+% calculate the head locations, index dir with data, rep it and cumsum
+tlocations = zeros(size(hlocations)); % make array for tail
 
 for idx = 2:size(tlocations,1)
-	newH = hlocations(idx,:);
-    currentT = tlocations(idx - 1,:);
-    posDelta = diff([currentT; newH]);
+	newH = hlocations(idx,:); %get new head
+    currentT = tlocations(idx - 1,:); % get old tail
+    posDelta = diff([currentT; newH]); % get diff of new head and old tail
     if all(abs(posDelta) < 2) % no move needed
         tlocations(idx,:) = tlocations(idx - 1,:);
     elseif any(posDelta == 0) % need to move U,D,L,R
