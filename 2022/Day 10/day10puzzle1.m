9a91947e48cd44123e51a25ec88c76aa6d817ed4
Daniel Breslan <danielbreslan@Francescas-MacBook-Air.local>
2022-12-10 11:07:10 +0000

    First solution

--- 2022/Day 10/day10puzzle1.m
+++ 2022/Day 10/day10puzzle1.m
@@ -1,5 +1,9 @@
 %% day10puzzle1 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").replace(["noop" "addx "],["1 0" "2 "])...
+    .split(" ").double();
+data(:,1) = cumsum(data(:,1));
 
+idx = [20 60 100 140 180 220]';
 
-day10puzzle1result = 0;
+day10puzzle1result = sum((sum((repmat(data(:,1)',numel(idx),1) < idx) .*...
+    repmat(data(:,2)',numel(idx),1),2) + 1) .* idx)
--- 2022/Day 10/day10puzzle2.m
+++ 2022/Day 10/day10puzzle2.m
@@ -1,5 +1,20 @@
 %% day10puzzle2 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").replace(["noop" "addx "],["1 0" "2 "])...
+    .split(" ").double();
+data(:,1) = cumsum(data(:,1));
 
+sprPos = 2;
 
-day10puzzle2result = 0;
+r1 = repmat(' ',1,40*6);
+
+for idx = 1:max(data(:,1))
+    cStrPos =  sprPos + floor(idx/40) * 40;
+    if idx >= cStrPos - 1 && idx <= cStrPos + 1
+        r1(idx) = '#';
+    end
+    if ~isempty(data(data(:,1) == idx,2))
+        sprPos = sprPos + data(data(:,1) == idx,2);
+    end
+end
+
+reshape(r1(1:40*6),40,6)'
