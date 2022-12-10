b2ec5cb1bd8e42349abf78f503b1c7d82bc3db9a
Daniel Breslan <danielbreslan@Francescas-MacBook-Air.local>
2022-12-10 13:12:43 +0000

    Removed tables and implemnted outerjoin manually

--- 2022/Day 10/day10puzzle2.m
+++ 2022/Day 10/day10puzzle2.m
@@ -4,22 +4,21 @@ data = readlines("input.txt").replace(["noop" "addx "],["1 0" "2 "])...
 data = cumsum(data); 
 data(:,2) = data(:,2) + 2; % add 2 - matlab is 1 based and to centre sprite
 
-data = array2table(data,VariableNames=["cycle","endreg"]); % tablulate data
-a = table((1:40*6)',VariableNames="cycle"); % Table of all cycles
-t = outerjoin(a,data,"RightVariables","endreg"); % join the data to all cyc
-t.endreg(1) = 2; % fist register is 2
-t.endreg = fillmissing(t.endreg,'previous'); % fill missing to move registe
-% location to all cycles
-t = t.Variables; % top using table, not needed now.
+a = [(1:240)' nan(240,1)]; % make a list of all cycles
+data = [data; a(~ismember(a(:,1),data(:,1)),:)]; % remove cycles in data
+[~,b] = sort(data(:,1)); %sort in order of cycles
+data = data(b,:);
+data(1,2) = 2; % set first register to 2 - ML is 1 based and centre sprite
+data(:,2) = fillmissing(data(:,2),"previous"); % copy missing regs down
 
-t = [t [2;t(1:end-1,2)]]; % make start of cycle register location
-t(:,1) = mod(t(:,1),40); % max is 40 as per row
-t(t(:,1) == 0,1) = 40; % mod makes 40 == 0, change back to 40
+data = [data [2;data(1:end-1,2)]]; % make start of cycle register location
+data(:,1) = mod(data(:,1),40); % max is 40 as per row
+data(data(:,1) == 0,1) = 40; % mod makes 40 == 0, change back to 40
 
-t(:,4) = t(:,1) >= t(:,3) - 1 & t(:,1) <= t(:,3) + 1; % see if cycle is +/-
-% 1 from register location (civered by sprite)
+data(:,4) = data(:,1) >= data(:,3) - 1 & data(:,1) <= data(:,3) + 1; % see
+% if cycle is +/-1 from register location (covered by sprite)
 res = repmat(' ',40,6); % make the output as blank
-res(t(1:40*6,4) == 1) = '#'; % fill in the #, where needed
+res(data(1:40*6,4) == 1) = '#'; % fill in the #, where needed
 day10puzzle2result = reshape(res,40,6)' %#ok<NOPTS> % resize
 
 
