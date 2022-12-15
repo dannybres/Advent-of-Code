2f6ae1815ede85be62e41e1488a31d2425d37163
Daniel Breslan <daniel@breslan.co.uk>
2022-12-15 10:39:11 +0000

    mid way

--- 2022/Day 15/day15puzzle1.m
+++ 2022/Day 15/day15puzzle1.m
@@ -1,5 +1,28 @@
 %% day15puzzle1 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").erase(["Sensor at x=", " y=",...
+    "closest beacon is at x="]).replace(": ",",").split(",").double();
+r = 2000000;
+% r = 10;
 
+t = array2table(data,VariableNames=["sx","sy","bx","by"]);
+t = [t table(abs(t.sy-t.by) + abs(t.sx-t.bx),VariableNames="md")];
+from = t.sx - t.md + abs(t.sy-r);
+to = t.sx + t.md - abs(t.sy-r);
+from(from > to) = nan;
+to(isnan(from)) = nan;
+t = [t table(from, to)];
 
-day15puzzle1result = 0;
+ranges = t{~isnan(t.to),["from","to"]};
+
+res = ranges(1,:);
+for idx = 2:size(ranges,1)
+    thisRange = ranges(idx,:);
+    overlap = any([thisRange(1) <= res(:,2) thisRange(2) >= res(:,1)]);
+    allRangesExisting = res(overlap,:);
+    allRangesExisting = [allRangesExisting(:); thisRange(:)];
+    res(overlap,:) = [];
+    res = [res; [min(allRangesExisting) max(allRangesExisting)]]; %#ok<AGROW> 
+end
+
+unBea = unique(t{t.by == r,["bx","by"]},"rows");
+day15puzzle1result = res(2) - res(1) + 1 -size(unBea,1) %#ok<NOPTS> 
--- 2022/Day 15/day15puzzle2.m
+++ 2022/Day 15/day15puzzle2.m
@@ -1,5 +1,36 @@
 %% day15puzzle2 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").erase(["Sensor at x=", " y=",...
+    "closest beacon is at x="]).replace(": ",",").split(",").double();
 
+sz = 4000000;
+for r = 97100:sz
+    if mod(r,1000) == 0
+        disp(r)
+    end
+    t = array2table(data,VariableNames=["sx","sy","bx","by"]);
+    t = [t table(abs(t.sy-t.by) + abs(t.sx-t.bx),VariableNames="md")];
+    from = t.sx - t.md + abs(t.sy-r);
+    to = t.sx + t.md - abs(t.sy-r);
+    from(from > to) = nan;
+    to(isnan(from)) = nan;
+    t = [t table(from, to)];
 
-day15puzzle2result = 0;
+    ranges = t{~isnan(t.to),["from","to"]};
+
+    res = ranges(1,:);
+    for idx = 2:size(ranges,1)
+        thisRange = ranges(idx,:);
+        overlap = (res(:,1) <= thisRange(1) & res(:,2) >= thisRange(1))|...
+            (res(:,1) <= thisRange(2) & res(:,2) >= thisRange(2))|...
+            (thisRange(1) < res(:,1) & thisRange(2) > res(:,2));
+        allRangesExisting = res(overlap,:);
+        allRangesExisting = [allRangesExisting(:); thisRange(:)];
+        res(overlap,:) = [];
+        res = [res; [min(allRangesExisting) max(allRangesExisting)]]; %#ok<AGROW>
+    end
+    if size(res,1) > 1
+        res
+        break
+    end
+end
+(res(1,2) + 1) * 4000000 + r
