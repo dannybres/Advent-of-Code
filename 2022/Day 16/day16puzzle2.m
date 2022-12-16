36b2eea05fa92cabd90be4606695ef6fd9162866
Daniel Breslan <danielbreslan@Francescas-MacBook-Air.local>
2022-12-16 23:04:33 +0000

    mid

--- 2022/Day 16/day16puzzle2.m
+++ 2022/Day 16/day16puzzle2.m
@@ -1,17 +1,90 @@
 %% day16puzzle1 - Daniel Breslan - Advent Of Code 2022
 [vlu,dlu,allVals] = getData("inputDemo.txt");
-% allVals = (1:4)'
+tLimit = 30;
+
 pathsMe = [repelem(allVals(1),numel(allVals).^2,1) repelem(allVals,numel(allVals),1)]; %make path to V1
 pathsEle = [repelem(allVals(1),numel(allVals).^2,1) repmat(allVals,numel(allVals),1)]; %make path to V1
-t = dlu(pathsMe(:,end-1) + pathsMe(:,end)) + 1; % calc t to open v1
-pres = (30 - t) .* vlu(pathsMe(:,end)); %calc p from these v1
+
+combo = [pathsMe pathsEle(:, 2:end)]
+
+keep = false(numel(combo),1);
+for idx = 1:size(combo,1)
+	keep(idx) = numel(unique(combo(idx,:))) == 2 * size(pathsMe,2) - 1;
+end
+
+pathsMe = pathsMe(keep,:);
+pathsEle  = pathsEle(keep,:);
+
+
+tMe = dlu(pathsMe(:,end-1) + pathsMe(:,end)) + 1; % calc t to open v1
+presMe = (30 - tMe) .* vlu(pathsMe(:,end)); %calc p from these v1
+
+tEle = dlu(pathsEle(:,end-1) + pathsEle(:,end)) + 1; % calc t to open v1
+presEle = (30 - tEle) .* vlu(pathsEle(:,end)); %calc p from these v1
+
+
+tpresIdx = presMe + presEle > max(presMe + presEle) * .75;
+
+pathsMe = pathsMe(tpresIdx, :);
+tMe = tMe(tpresIdx);
+presMe = presMe(tpresIdx);
+
+pathsEle = pathsEle(tpresIdx, :);
+tEle = tEle(tpresIdx);
+presEle = presEle(tpresIdx);
+
+
+
+
+maxSoFar = 0; %keep track of Max Pressure
+% while 1 %loooooooop
+newMe = repmat(repmat(allVals,numel(allVals),1),size(pathsMe,1),1);
+newEle = repmat(repelem(allVals,numel(allVals),1),size(pathsMe,1),1);
+pathsMe = [repelem(pathsMe,numel(allVals).^2,1) newMe];
+pathsEle = [repelem(pathsEle,numel(allVals).^2,1) newEle];
+
+tMe = repelem(tMe,numel(allVals).^2,1);
+presMe = repelem(presMe,numel(allVals).^2,1);
+tEle = repelem(tEle,numel(allVals).^2,1);
+presEle = repelem(presEle,numel(allVals).^2,1);
+combo = [pathsMe pathsEle(:, 2:end)];
+
+keep = false(numel(combo),1);
+for idx = 1:size(combo,1)
+	keep(idx) = numel(unique(combo(idx,:))) == 2 * size(pathsMe,2) - 1;
+end
+
+pathsMe = pathsMe(keep, :);
+tMe = tMe(keep);
+presMe = presMe(keep);
+
+pathsEle = pathsEle(keep, :);
+tEle = tEle(keep);
+presEle = presEle(keep);
+
+tMe = tMe + dlu(pathsMe(:,end-1) + pathsMe(:,end)) + 1; % calc t to open v1
+presMe = presMe + (30 - tMe) .* vlu(pathsMe(:,end)); %calc p from these v1
+
+tEle = tEle + dlu(pathsEle(:,end-1) + pathsEle(:,end)) + 1; % calc t to open v1
+presEle = presEle + (30 - tEle) .* vlu(pathsEle(:,end)); %calc p from these v1
+
+tpresIdx = presMe + presEle > max(presMe + presEle) * .75;
+
+pathsMe = pathsMe(tpresIdx, :);
+tMe = tMe(tpresIdx);
+presMe = presMe(tpresIdx);
+
+pathsEle = pathsEle(tpresIdx, :);
+tEle = tEle(tpresIdx);
+presEle = presEle(tpresIdx);
+
+pathsEle(presMe + presEle == max(presMe + presEle),:)
 
 return
 
-tLimit = 30
+tpresIdx = presMe + presEle > max(presMe + presEle) * .75;
 
-maxSoFar = 0; %keep track of Max Pressure
-while 1 %loooooooop
+return
     newEle = repmat(allVals,numel(t),1); % new places (all valves)
     pathsMe = repelem(pathsMe,numel(allVals),1); % suplicate paths for new v
     pathsMe = [pathsMe newEle]; % add lest valve
@@ -41,9 +114,9 @@ while 1 %loooooooop
 
     mmm = pres(pres == max(pres) & t <=30); %update max pressure
     if isempty(mmm)
-        break
+%         break
     else
         maxSoFar = max(maxSoFar, mmm);
     end
-end
+% end
 day16puzzle1result = maxSoFar %#ok<NOPTS>
