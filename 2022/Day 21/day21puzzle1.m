7446de42f3e524757468fbe85d15c63f98ff8a04
Daniel Breslan <daniel@breslan.co.uk>
2022-12-21 14:30:38 +0000

    Part 1 done.

--- 2022/Day 21/day21puzzle1.m
+++ 2022/Day 21/day21puzzle1.m
@@ -1,5 +1,26 @@
 %% day21puzzle1 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(": ");
 
+nums = data(strlength(data(:,2))< 8,:);
+equa = data(strlength(data(:,2))> 8,:);
 
-day21puzzle1result = 0;
+funlu = dictionary(["+","-","*","/"],{@(x) x(1)+x(2),@(x) x(1)-x(2), ...
+    @(x) x(1)*x(2),@(x) x(1)/x(2)});
+
+monkeys = dictionary(nums(:,1), nums(:,2).double());
+
+while ~isKey(monkeys,"root")
+    needed = [equa(:,2).extractBefore(5) equa(:,2).extractAfter(7)];
+    solvable = all(ismember(needed,monkeys.keys),2);
+    canbesolved = equa(solvable,:);
+    monkeys([canbesolved(:,2).extractBefore(5) canbesolved(:,2)...
+        .extractAfter(7)]);
+    funs = funlu(canbesolved(:,2).extract(6));
+    for idx = 1:height(canbesolved)
+    	fun = funs{idx};
+        monkeys(canbesolved(idx,1)) = fun(monkeys([canbesolved(idx,2)...
+            .extractBefore(5) canbesolved(idx,2).extractAfter(7)]));
+    end
+    equa = equa(~solvable,:);
+end
+day21puzzle1result = monkeys("root")
--- 2022/Day 21/day21puzzle2.m
+++ 2022/Day 21/day21puzzle2.m
@@ -1,5 +1,38 @@
 %% day21puzzle2 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(": ");
+nums = data(strlength(data(:,2))< 8,:);
+equa = data(strlength(data(:,2))> 8,:);
+
+funlu = dictionary(["+","-","*","/"],{@(x) x(1)+x(2),@(x) x(1)-x(2), ...
+    @(x) x(1)*x(2),@(x) x(1)/x(2)});
+humn = 0;
+passesTest = false;
+while ~passesTest
+    if mod(humn,10) == 0
+        disp(humn)
+    end
+    monkeys = dictionary(nums(:,1), nums(:,2).double());
+    equa = data(strlength(data(:,2))> 8,:);
+    humn = humn + 1;
+    monkeys("humn") = humn;
+    while true
+        needed = [equa(:,2).extractBefore(5) equa(:,2).extractAfter(7)];
+        solvable = all(ismember(needed,monkeys.keys),2);
+        canbesolved = equa(solvable,:);
+        if any(canbesolved(:,1) == "root")
+            passesTest = diff(monkeys([canbesolved(canbesolved(:,1) == "root",2).extractBefore(5) canbesolved(canbesolved(:,1) == "root",2)...
+                .extractAfter(7)])) == 0;
+            break
+        end
+        funs = funlu(canbesolved(:,2).extract(6));
+        for idx = 1:height(canbesolved)
+        	fun = funs{idx};
+            monkeys(canbesolved(idx,1)) = fun(monkeys([canbesolved(idx,2)...
+                .extractBefore(5) canbesolved(idx,2).extractAfter(7)]));
+        end
+        equa = equa(~solvable,:);
+    end
+end
 
 
-day21puzzle2result = 0;
+day21puzzle2result = monkeys("humn")
