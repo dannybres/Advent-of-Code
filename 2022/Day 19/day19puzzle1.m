cc021bdd26a0e38facd4a5f27d2afa69234055a1
Daniel Breslan <daniel@breslan.co.uk>
2022-12-21 10:04:29 +0000

    Optimised the hell out of it - p2 complete.

--- 2022/Day 19/day19puzzle1.m
+++ 2022/Day 19/day19puzzle1.m
@@ -1,6 +1,6 @@
-%% day19puzzle1 - Daniel Breslan - Advent Of Code 2022
-clc
-tic
+% day19puzzle1 - Daniel Breslan - Advent Of Code 2022
+% profile on
+clear
 data = readlines("input.txt");
 [~,blueprints] = regexp(data,"Blueprint \d+: Each ore robot costs" + ...
     " (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian" + ...
@@ -9,116 +9,78 @@ data = readlines("input.txt");
     'tokens'); % Parse input
 blueprints = vertcat(blueprints{:});
 blueprints = vertcat(blueprints{:});
-global cache
-
+tic
 results = zeros(1,height(blueprints));
 for idx = 1:height(blueprints)
-    blueprint.ore.ore = blueprints(idx,1).double; % Define ini conditions
-    blueprint.clay.ore = blueprints(idx,2).double;
-    blueprint.obsidian.ore = blueprints(idx,3).double;
-    blueprint.obsidian.clay = blueprints(idx,4).double;
-    blueprint.geode.ore = blueprints(idx,5).double;
-    blueprint.geode.obsidian = blueprints(idx,6).double;
+    blueprint{1} = [blueprints(idx,1).double 0 0 0];
+    blueprint{2} = [blueprints(idx,2).double 0 0 0];
+    blueprint{3} = [blueprints(idx,3).double blueprints(idx,4).double 0 0];
+    blueprint{4} = [blueprints(idx,5).double 0 blueprints(idx,6).double 0];
 
-    maxVals.ore = max(blueprints(idx,[1 2 3 5]).double);
-    maxVals.clay = blueprints(idx,4).double;
-    maxVals.obsidian = blueprints(idx,6).double;
-    maxVals.geode = Inf;
+    maxVals = [max(blueprints(idx,[1 2 3 5]).double) ...
+        blueprints(idx,4).double ...
+        blueprints(idx,6).double ...
+        Inf];
 
-    %% t0
-    resources.ore = 0;
-    resources.clay = 0;
-    resources.obsidian = 0;
-    resources.geode = 0;
-
-    robots.ore = 1;
-    robots.clay = 0;
-    robots.obsidian = 0;
-    robots.geode = 0;
-
+    resources = zeros(1,4);
+    robots = [1 0 0 0];
     tRemaining = 24;
 
-    cache = dictionary(string.empty,double.empty);
-    
     results(idx) = search(resources, robots, tRemaining, blueprint, ...
-        maxVals);% run
-    
+        maxVals, 0);% run
 end
 
 day19puzzle1result = sum(results .* (1:numel(results)))
 toc
+% profile viewer
+
 function maxGeodes = search(resources, robots, tRemaining, blueprint, ...
-    maxVals)
-cKey = resources.ore + "_" + resources.clay + "_" + ...
-    resources.obsidian + "_" + robots.ore + "_" + ...
-    robots.clay + "_" + robots.obsidian + "_" + robots.geode +...
-    "_" + tRemaining;
-global cache
-if isKey(cache,cKey)
-    maxGeodes = cache(cKey);
+    maxVals, maxGeodesSoFar)
+
+maxGeodes = resources(end);
+
+if maxGeodesSoFar >= resources(end) + (robots(end) * tRemaining)...
+        + ((tRemaining * (tRemaining-1)) / 2)
     return
 end
-robotTypes = string(fieldnames(blueprint)); % What robots can you build?
-robotsAvailableToBuild = robotTypes(1:min(4,(sum(cellfun(@(x)(robots.(x))...
-    ,fieldnames(robots)) ~= 0)+1)));
 
-maxGeodes = resources.geode;
+robotsAvailableToBuild = min(4,nnz(robots)+1);
 
-% if robots.geode ~= 0
-%     1
-% end
-if numel(robotsAvailableToBuild) == 0
-    error("no robots to build")
-end
-for robIdx = 1:numel(robotsAvailableToBuild) % Next robot to build
-    if robots.(robotsAvailableToBuild(robIdx)) == maxVals.( ...
-            robotsAvailableToBuild(robIdx))
+for robIdx = robotsAvailableToBuild(end):-1:1 % Next robot to build
+    if robots(robIdx) == maxVals(robIdx)
         continue
     end
     iresources = resources;
     irobots = robots;
+    neededToBuildRobot = blueprint{robIdx};
 
-    neededToBuildRobot = blueprint.(robotsAvailableToBuild(robIdx));
-    resourcesNeeded = string(fieldnames(neededToBuildRobot));
     daysToWait = 0;
-    for resIdx = 1:numel(resourcesNeeded) % loop res and calc longest wait
-        totalNumberOfThisResourceNeeded = neededToBuildRobot...
-            .(resourcesNeeded(resIdx));
+    
+    for resIdx = 1:numel(neededToBuildRobot) % loop res and calc longest wait
+        if neededToBuildRobot(resIdx) == 0
+            continue
+        end
+        totalNumberOfThisResourceNeeded = neededToBuildRobot(resIdx);
         needToCollect = totalNumberOfThisResourceNeeded - ...
-            iresources.(resourcesNeeded(resIdx));
-        daysToWait = max(daysToWait, ceil(needToCollect/...
-            irobots.(resourcesNeeded(resIdx))));
+            iresources(resIdx);
+        daysToWait = max(daysToWait, ceil(needToCollect/irobots(resIdx)));
     end
     daysToWait = daysToWait + 1;
-    if daysToWait < 1
-        1
-    end
 
     if tRemaining - daysToWait <= 0 % no time to bld the robot (cap time)
-        maxGeodes = max(maxGeodes, iresources.geode + irobots.geode * ...
+        maxGeodes = max(maxGeodes, iresources(end) + irobots(end) * ...
             tRemaining);
         continue
     end
     % update resources based on days waiting
-    iresources.ore = iresources.ore + irobots.ore * daysToWait;
-    iresources.clay = iresources.clay + irobots.clay * daysToWait;
-    iresources.obsidian = iresources.obsidian + irobots.obsidian * 
-    daysToWait;
-    iresources.geode = iresources.geode + irobots.geode * daysToWait;
+    iresources = iresources + irobots * daysToWait;
 
-    for resIdx = 1:numel(resourcesNeeded) % remove resources
-        totalNumberOfThisResourceNeeded = neededToBuildRobot...
-            .(resourcesNeeded(resIdx));
-        iresources.(resourcesNeeded(resIdx)) = iresources...
-            .(resourcesNeeded(resIdx)) - ...
-            totalNumberOfThisResourceNeeded;
-    end
-    irobots.(robotsAvailableToBuild(robIdx)) =... % build it
-        irobots.(robotsAvailableToBuild(robIdx)) + 1;
+    % remove resources to build
+    iresources = iresources - neededToBuildRobot;
+    
+    % build it
+    irobots(robIdx) = irobots(robIdx) + 1;
     maxGeodes = max(maxGeodes, search(iresources, irobots, ...
-        tRemaining - daysToWait, blueprint, maxVals));
-
-
+        tRemaining - daysToWait, blueprint, maxVals, maxGeodes));
 end
-cache(cKey) = maxGeodes;
 end
--- 2022/Day 19/day19puzzle2.m
+++ 2022/Day 19/day19puzzle2.m
@@ -1,5 +1,86 @@
-%% day19puzzle2 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+% day19puzzle1 - Daniel Breslan - Advent Of Code 2022
+% profile on
+clear
+data = readlines("input.txt");
+[~,blueprints] = regexp(data,"Blueprint \d+: Each ore robot costs" + ...
+    " (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian" + ...
+    " robot costs (\d+) ore and (\d+) clay. Each geode robot" + ...
+    " costs (\d+) ore and (\d+) obsidian.", 'match', ...
+    'tokens'); % Parse input
+blueprints = vertcat(blueprints{:});
+blueprints = vertcat(blueprints{:});
+tic
+results = zeros(1,height(blueprints));
+for idx = 1:3%height(blueprints)
+    blueprint{1} = [blueprints(idx,1).double 0 0 0];
+    blueprint{2} = [blueprints(idx,2).double 0 0 0];
+    blueprint{3} = [blueprints(idx,3).double blueprints(idx,4).double 0 0];
+    blueprint{4} = [blueprints(idx,5).double 0 blueprints(idx,6).double 0];
 
+    maxVals = [max(blueprints(idx,[1 2 3 5]).double) ...
+        blueprints(idx,4).double ...
+        blueprints(idx,6).double ...
+        Inf];
 
-day19puzzle2result = 0;
+    resources = zeros(1,4);
+    robots = [1 0 0 0];
+    tRemaining = 32;
+
+    results(idx) = search(resources, robots, tRemaining, blueprint, ...
+        maxVals, 0);% run
+end
+
+day19puzzle1result = prod(results(1:3))
+toc
+% profile viewer
+
+function maxGeodes = search(resources, robots, tRemaining, blueprint, ...
+    maxVals, maxGeodesSoFar)
+
+maxGeodes = resources(end);
+
+if maxGeodesSoFar >= resources(end) + (robots(end) * tRemaining)...
+        + ((tRemaining * (tRemaining-1)) / 2)
+    return
+end
+
+robotsAvailableToBuild = min(4,nnz(robots)+1);
+
+for robIdx = robotsAvailableToBuild(end):-1:1 % Next robot to build
+    if robots(robIdx) == maxVals(robIdx)
+        continue
+    end
+    iresources = resources;
+    irobots = robots;
+    neededToBuildRobot = blueprint{robIdx};
+
+    daysToWait = 0;
+    
+    for resIdx = 1:numel(neededToBuildRobot) % loop res and calc longest wait
+        if neededToBuildRobot(resIdx) == 0
+            continue
+        end
+        totalNumberOfThisResourceNeeded = neededToBuildRobot(resIdx);
+        needToCollect = totalNumberOfThisResourceNeeded - ...
+            iresources(resIdx);
+        daysToWait = max(daysToWait, ceil(needToCollect/irobots(resIdx)));
+    end
+    daysToWait = daysToWait + 1;
+
+    if tRemaining - daysToWait <= 0 % no time to bld the robot (cap time)
+        maxGeodes = max(maxGeodes, iresources(end) + irobots(end) * ...
+            tRemaining);
+        continue
+    end
+    % update resources based on days waiting
+    iresources = iresources + irobots * daysToWait;
+
+    % remove resources to build
+    iresources = iresources - neededToBuildRobot;
+    
+    % build it
+    irobots(robIdx) = irobots(robIdx) + 1;
+    maxGeodes = max(maxGeodes, search(iresources, irobots, ...
+        tRemaining - daysToWait, blueprint, maxVals, maxGeodes));
+end
+end
