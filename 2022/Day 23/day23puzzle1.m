a6ccf6ba5a83b3b9253a74e5729c52906a9ccb9b
Daniel Breslan <daniel@breslan.co.uk>
2022-12-28 22:31:03 +0000

    Day 23 - all done! :)

--- 2022/Day 23/day23puzzle1.m
+++ 2022/Day 23/day23puzzle1.m
@@ -1,5 +1,67 @@
 %% day23puzzle1 - Daniel Breslan - Advent Of Code 2022
-data = readlines("inputDemo.txt")
+data = char(readlines("input.txt")) == '#';
+[elvesr, elevesc] = ind2sub(size(data),find(data));
+elves = [elvesr elevesc];
+lastElves = elves * 0;
+directions(:,:,:,1) = reshape([-1 -1 -1; -1:1],1,2,[]);
+directions(:,:,:,2) = reshape([1 1 1; -1:1],1,2,[]);
+directions(:,:,:,3) = reshape([-1:1; -1 -1 -1],1,2,[]);
+directions(:,:,:,4) = reshape([-1:1; 1 1 1],1,2,[]);
+
+allDirs = [repelem((-1:1)',3,1) repmat((-1:1)',3,1)];
+allDirs(ismember(allDirs,[0 0],'rows'),:) = [];
+allDirs = reshape(allDirs',1,2,[]);
+
+dirModder = 0;
+
+for idx = 1:10
+    proposedMoves = nan(size(elves));
+
+    finishedElf = all(reshape(~ismember(reshape(pagetranspose(elves ...
+        + allDirs),2,[])',elves,'rows'), [],8),2);
+
+    for idx = 1:size(directions,4)
+
+        dirIdx = mod(idx + dirModder,4);
+        dirIdx(dirIdx == 0) = 4;
+        direction = directions(:,:,:,dirIdx);
+        options = elves(~finishedElf,:) + direction;
+
+        options = options(isnan(proposedMoves(~finishedElf,1)),:,:);
+        
+        potentialElves = find(all(isnan(proposedMoves),2) & ~finishedElf);
+
+        canMoveInDirection = all(reshape(~ismember(reshape( ...
+            pagetranspose(options),2,[])',elves,'rows'), [],3),2);
+
+        elvesToUpdate = potentialElves(canMoveInDirection);
+        
+        movesToAdd = options(canMoveInDirection,:,2);
+
+        proposedMoves(elvesToUpdate,:) = movesToAdd;
 
 
-day23puzzle1result = 0;
+
+        if all(~isnan(proposedMoves),'all')
+            break
+        end
+    end
+
+    proposedMovesId = 2 * proposedMoves(:,1) * max(proposedMoves,[], ...
+        "all") + proposedMoves(:,2);
+
+    elvesCanMove = sum(proposedMovesId == proposedMovesId',2) == 1;
+    elves(elvesCanMove & ~finishedElf,:) = proposedMoves(elvesCanMove ...
+        & ~finishedElf,:);
+    dirModder = dirModder + 1;
+
+
+    if all(lastElves == elves,'all')
+        break
+    end
+    lastElves = elves;
+
+end
+
+day23puzzle1result = prod(max(elves) - min(elves) +1) - ...
+size(elves,1) %#ok<NOPTS> 
