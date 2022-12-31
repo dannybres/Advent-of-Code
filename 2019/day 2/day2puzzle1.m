1b431e3000e6a5b2da503422006f812c435fe1c8
Daniel Breslan <daniel@breslan.co.uk>
2022-12-31 13:13:13 +0000

    19, day 2 reimplemented

--- 2019/day 2/day2puzzle1.m
+++ 2019/day 2/day2puzzle1.m
@@ -1,26 +1,18 @@
 %% day2puzzle1 - Daniel Breslan - Advent Of Code 2019
-dat = readlines("input.txt").split(",").double();
+data = readlines("input.txt").split(",").double();
 
-% change vals
-dat(2) = 12;
-dat(3) = 2;
+data(2:3) = [12 2];
 
-% trip before 99 in position 1
-endIdx = find(dat(1:4:end) == 99);
-endIdx = 4 * (endIdx(1)-1);
-prodat = dat(1:endIdx);
-
-% reshape and loop actions
-prodat = reshape(prodat,4,[])';
-for idx = 1:size(prodat,1)
-    datEle = prodat(idx,:);
-    datEle(2:end) = datEle(2:end) + 1;
-    fun = @prod;
-    if datEle(1) == 1
-        fun = @sum;
+for idx = 1:4:numel(data)
+    switch data(idx)        
+        case 1
+            fun = @sum;
+        case 2
+            fun = @prod;
+        case 99
+            break
     end
-    dat(datEle(4)) = fun(dat(datEle(2:3)));
+    data(data(idx+3)+1) = fun(data(data(idx+(1:2))+1));
 end
 
-    
-day2puzzle1result = dat(1)
+day2puzzle1result = data(1) %#ok<NOPTS> 
--- 2019/day 2/day2puzzle2.m
+++ 2019/day 2/day2puzzle2.m
@@ -1,41 +1,24 @@
-%% day2puzzle1 - Daniel Breslan - Advent Of Code 2019
-dat = readlines("input.txt").split(",").double();
-ogDat = dat;
-for noun = 0:99
-    for verb = 0:99
-        dat = ogDat;
-        % change vals
-        dat(2) = noun;
-        dat(3) = verb;
+%% day2puzzle2 - Daniel Breslan - Advent Of Code 2019
+% Instruction(instriction pointer) > opcode and 3 parameter
 
-        % trip before 99 in position 1
-        endIdx = find(dat(1:4:end) == 99);
-        endIdx = 4 * (endIdx(1)-1);
-        prodat = dat(1:endIdx);
-
-        % all numbers = memory
-        % position is address e.g. 1st is address 0
-        % 1st in 4 group is an instruction
-        % 2nd to 4th are parameters
-
-        % reshape and loop actions
-        prodat = reshape(prodat,4,[])';
-        for idx = 1:size(prodat,1)
-            datEle = prodat(idx,:);
-            datEle(2:end) = datEle(2:end) + 1;
-            fun = @prod;
-            if datEle(1) == 1
+dataOriginal = readlines("input.txt").split(",").double();
+options = [repelem(0:99,1,99); repmat(0:99,1,99)];
+for optIdx = options
+    data = dataOriginal;
+    data(2:3) = optIdx;
+    for idx = 1:4:numel(data)
+        switch data(idx)
+            case 1
                 fun = @sum;
-            end
-            dat(datEle(4)) = fun(dat(datEle(2:3)));
+            case 2
+                fun = @prod;
+            case 99
+                break
         end
-        if dat(1) == 19690720
-            break
-        end
+        data(data(idx+3)+1) = fun(data(data(idx+(1:2))+1));
     end
-    if dat(1) == 19690720
+    if data(1) == 19690720
         break
     end
 end
-
-day2puzzle1result = 100 * noun + verb
+day2puzzle2result = sum(optIdx .* [100; 1]) %#ok<NOPTS> 
