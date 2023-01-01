3edd131f4b8f95e93ab21920029a2ad65458509c
Daniel Breslan <danielbreslan@Francescas-MacBook-Air.local>
2023-01-01 19:03:49 +0000

    19.5a&b done

--- 2019/day 5.1/day5puzzle2.m
+++
@@ -1,5 +0,0 @@
-%% day5puzzle2 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
-
-
-day5puzzle2result = 0;
--- 2019/day 5/day5puzzle1.m
+++ 2019/day 5/day5puzzle1.m
@@ -1,78 +1,47 @@
 %% day5puzzle1 - Daniel Breslan - Advent Of Code 2019
-clc
+% Instruction(instriction pointer) > opcode and 3 parameter
+data = readlines("input.txt").split(",").double();
 
-data = readlines("input.txt");
-data = double(data.split(","));
+input = 1;
 
-numberOfParametersForIns = [3 3 1 1 2 2 3 3];
+day2puzzle1result = runIntcode(data,input); %#ok<NOPTS> 
 
-start = 1;
-input = 5;
+function result = runIntcode(data,input)
 
-% disp(data')
-
-while start < numel(data)
-    clear mode
-    stopAdvance = false;
-    instruction = data(start);
-    if instruction < 100
-        opcode = instruction;
-    else
-        opcode = rem(instruction,100);
-        mode = floor(instruction/100);
+idx = 1;
+while 1
+    [opcode, modes] = decryptInstruction(data(idx));
+    
+    if opcode == 9
+        break
     end
-    if opcode == 99
-        return
-    end
-    lengthOfParam = numberOfParametersForIns(opcode);
-    if ~exist("mode","var")
-        indexes = data(start+1:start+min(2,lengthOfParam));
-        parameters = data(indexes + 1);
-    else
-        immediate = num2str(mode) == '1';
-        immediate = padarray(immediate',min(2,lengthOfParam) ...
-            - numel(immediate),false,'pre')';
-        immediate = fliplr(immediate);
-        indexes = data(start+1:start+min(2,lengthOfParam));
-        parameters = data(min(8,max(0,indexes)) + 1);
-        parameters = indexes' .* immediate + ...
-            parameters' .* ~immediate;
-    end
+    
+    parameters = data(idx+(1:numel(modes)));
+    parameters(~modes) = data(parameters(~modes)+1);
 
-    if opcode == 1
-        data(data(start+3)+1) = sum(parameters);
-    elseif opcode == 2
-        data(data(start+3)+1) = prod(parameters);
-    elseif opcode == 3
-        data(indexes + 1) = input;
-    elseif opcode == 4
-        disp(parameters)
-    elseif opcode == 5
-        if parameters(1)
-            start = parameters(2) + 1;
-            continue
-        end
-    elseif opcode == 6
-        if ~parameters(1)
-            start = parameters(2) + 1;
-            continue
-        end
-    elseif opcode == 7
-        data(data(start+3)+1) = diff(parameters) > 0;
-        stopAdvance = data(start+3)+1 == start;
-    elseif opcode == 8
-        data(data(start+3)+1) = diff(parameters) == 0;
-        stopAdvance = data(start+3)+1 == start;
-    else
-        error('000')
+    switch opcode
+        case 1 
+            data(1 + data(idx+3)) = sum(parameters(1:2));
+        case 2
+            data(1 + data(idx+3)) = prod(parameters(1:2));
+        case 3 % input and saves it
+            data(1 + data(idx+1)) = input;
+        case 4 % outputs the value
+            disp("output is " + parameters)
     end
-
-    if stopAdvance
-        stopAdvance
-    end
-
-    start = start + lengthOfParam + 1;
-%     disp(data')
+    idx = idx + numel(modes) + 1;
+    
+end
+result = data;
 end
 
-day5puzzle1result = 0;
+function [opcode, modes] = decryptInstruction(opcode)
+   opcodeString = string(opcode);
+   opcode = opcodeString.extractAfter(opcodeString.strlength-1).double;
+   numberOfParameters = 3;
+   if opcode > 2
+       numberOfParameters = 1;
+   end
+   modes = fliplr(char(pad(opcodeString.extractBefore(max(1, ...
+       opcodeString.strlength-1)),numberOfParameters,"left",'0')) == '1');
+end
