bacb8eb45d763bdcb1831267aa7c3d80d5f8712f
Daniel Breslan <daniel@breslan.co.uk>
2023-01-03 10:34:44 +0000

    Finished 19.8

--- 2019/day 8/day8puzzle1.m
+++ 2019/day 8/day8puzzle1.m
@@ -1,5 +1,13 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+w = 25;
+h = 6;
 
+data = readlines("input.txt").split("");
+data = data(2:end-1).double;
+data = reshape(data,w*h,[]);
 
-day8puzzle1result = 0;
+minLayer = find(sum(data == 0) == min(sum(data == 0)));
+
+prod(sum(data(:,minLayer) == [1 2]))
+
+day8puzzle1result = prod(sum(data(:,minLayer) == [1 2])) %#ok<NOPTS> 
--- 2019/day 8/day8puzzle2.m
+++ 2019/day 8/day8puzzle2.m
@@ -1,5 +1,16 @@
 %% day8puzzle2 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+w = 25;
+h = 6;
 
+data = readlines("input.txt").split("");
+data = data(2:end-1).double;
+data = data((reshape(1:numel(data),w*h,[]))');
 
-day8puzzle2result = 0;
+idx = data ~= 2;
+idx = idx .* (1:size(idx, 1)).';
+idx(idx == 0) = NaN;
+idx = min(idx, [], 1);
+
+pixels = data(sub2ind(size(data),idx,1:width(data)));
+pixels = reshape(pixels,w,h);
+imshow(repelem(pixels'==1,10,10))
