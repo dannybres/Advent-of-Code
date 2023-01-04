2cf17aee493caf61f966bc45f345f226a34ec8be
Daniel Breslan <danielbreslan@Francescas-MacBook-Air.local>
2023-01-04 21:29:40 +0000

    Day 19.10a and 19,10b done! :)

--- 2019/day 10/day10puzzle1.m
+++ 2019/day 10/day10puzzle1.m
@@ -1,16 +1,12 @@
 %% day10puzzle1 - Daniel Breslan - Advent Of Code 2019
 data = char(readlines("input.txt")) == '#';
-
 [r,c] = ind2sub(size(data),find(data));
-
 asteroids = [r,c];
 best = 0;
 for idx = 1:height(asteroids)
-
     searchField = asteroids;
     opt = searchField(idx,:);
     searchField(idx,:) = [];
-
     diff = searchField - opt;
     diff = complex(diff(:,1),diff(:,2));
     visible = numel(unique(angle(diff)));
--- 2019/day 10/day10puzzle2.m
+++ 2019/day 10/day10puzzle2.m
@@ -1,50 +1,50 @@
 %% day10puzzle2 - Daniel Breslan - Advent Of Code 2019
-data = char(readlines("inputDemo.txt")) == '#';
+data = char(readlines("input.txt")) == '#';
 
+% find laser location
 [r,c] = ind2sub(size(data),find(data));
-
 asteroids = [r,c];
 best = 0;
 bestIdx = 0;
-for idx = 1:height(asteroids)
-
+for idx = 1:height(asteroids) % loop all asteroids
     searchField = asteroids;
-    opt = searchField(idx,:);
-    searchField(idx,:) = [];
-
-    diff = searchField - opt;
-    diff = complex(diff(:,1),diff(:,2));
-    visible = numel(unique(angle(diff)));
+    opt = searchField(idx,:); % select one
+    searchField(idx,:) = []; % remove it
+    diff = searchField - opt; % get distance
+    diff = complex(diff(:,1),diff(:,2)); % complex vector from 1 to all
+    visible = numel(unique(angle(diff))); % find number of unique angles
     if visible > best
         best = visible;
         bestIdx = idx;
     end
 end
+coord = asteroids(bestIdx,:); % laser location
 
-coord = asteroids(bestIdx,:)';
-
-coord = [4 9];
-
-map = repmat(' ',size(data));
-map(sub2ind(size(data),r,c)) = '.';
-map(coord(1),coord(2)) = '#';
-
-allAsteroids = asteroids;
+allAsteroids = asteroids; % set up a table of all angles, dist, coord
 allAsteroids(bestIdx,:) = [];
-
 differences = coord - allAsteroids;
 differences = complex(differences(:,1),differences(:,2));
+allAsteroids = table(angle(differences), abs(differences), allAsteroids,...
+    nan(height(allAsteroids),1), ...
+    VariableNames = ["angle","dist","coo","order"]); % add order of destr
+allAsteroids.angle(allAsteroids.angle < 0) = 2*pi + allAsteroids.angle(...
+    allAsteroids.angle < 0);
+allAsteroids.angle = 2*pi - allAsteroids.angle;
+allAsteroids.angle(allAsteroids.angle == 2*pi) = 0; % cleanse data
+allAsteroids = sortrows(allAsteroids,["angle","dist"]); % sort it
 
-remainingAsteroids = table(angle(differences), abs(differences), allAsteroids, VariableNames = ["angle","dist","coo"])
-remainingAsteroids.angle(remainingAsteroids.angle < 0) = 2*pi + remainingAsteroids.angle(remainingAsteroids.angle < 0);
-remainingAsteroids.angle = 2*pi - remainingAsteroids.angle;
-remainingAsteroids.angle(remainingAsteroids.angle == 2*pi) = 0;
-
-allAsteroids = sortrows(remainingAsteroids,["angle","dist"])
-
-% allAsteroids = [allAsteroids nan(height(allAsteroids),1)]
-
-
-
-
-day10puzzle2result = best %#ok<NOPTS> 
+while any(isnan(allAsteroids.order))
+    availableToDestroy = allAsteroids(isnan(allAsteroids.order),:);
+    canBeDestroyedThisLoop = [true; availableToDestroy.angle(2:end) ~= ...
+        availableToDestroy.angle(1:end-1)]; % not the same as prior angle
+    % can be destroyed this loop.
+    availableToDestroy.order(canBeDestroyedThisLoop) = ...
+        1:sum(canBeDestroyedThisLoop);
+    if ~isnan(max(allAsteroids.order))
+        availableToDestroy.order = availableToDestroy.order + ...
+            max(allAsteroids.order);
+    end
+    allAsteroids(isnan(allAsteroids.order),:) = availableToDestroy;
+end
+rock200 = fliplr(allAsteroids.coo(allAsteroids.order == 200,:))-1;
+day10puzzle2result = sum(rock200 .* [100 1]) %#ok<NOPTS>
