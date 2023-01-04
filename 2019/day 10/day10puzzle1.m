f33495d9c60edd51b23943cc98752e87abb3a63d
Daniel Breslan <daniel@breslan.co.uk>
2023-01-04 20:52:05 +0000

    mid 10

--- 2019/day 10/day10puzzle1.m
+++ 2019/day 10/day10puzzle1.m
@@ -1,5 +1,21 @@
 %% day10puzzle1 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+data = char(readlines("input.txt")) == '#';
 
+[r,c] = ind2sub(size(data),find(data));
 
-day10puzzle1result = 0;
+asteroids = [r,c];
+best = 0;
+for idx = 1:height(asteroids)
+
+    searchField = asteroids;
+    opt = searchField(idx,:);
+    searchField(idx,:) = [];
+
+    diff = searchField - opt;
+    diff = complex(diff(:,1),diff(:,2));
+    visible = numel(unique(angle(diff)));
+    if visible > best
+        best = visible;
+    end
+end
+day10puzzle1result = best %#ok<NOPTS> 
--- 2019/day 10/day10puzzle2.m
+++ 2019/day 10/day10puzzle2.m
@@ -1,5 +1,50 @@
 %% day10puzzle2 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+data = char(readlines("inputDemo.txt")) == '#';
+
+[r,c] = ind2sub(size(data),find(data));
+
+asteroids = [r,c];
+best = 0;
+bestIdx = 0;
+for idx = 1:height(asteroids)
+
+    searchField = asteroids;
+    opt = searchField(idx,:);
+    searchField(idx,:) = [];
+
+    diff = searchField - opt;
+    diff = complex(diff(:,1),diff(:,2));
+    visible = numel(unique(angle(diff)));
+    if visible > best
+        best = visible;
+        bestIdx = idx;
+    end
+end
+
+coord = asteroids(bestIdx,:)';
+
+coord = [4 9];
+
+map = repmat(' ',size(data));
+map(sub2ind(size(data),r,c)) = '.';
+map(coord(1),coord(2)) = '#';
+
+allAsteroids = asteroids;
+allAsteroids(bestIdx,:) = [];
+
+differences = coord - allAsteroids;
+differences = complex(differences(:,1),differences(:,2));
+
+remainingAsteroids = table(angle(differences), abs(differences), allAsteroids, VariableNames = ["angle","dist","coo"])
+remainingAsteroids.angle(remainingAsteroids.angle < 0) = 2*pi + remainingAsteroids.angle(remainingAsteroids.angle < 0);
+remainingAsteroids.angle = 2*pi - remainingAsteroids.angle;
+remainingAsteroids.angle(remainingAsteroids.angle == 2*pi) = 0;
+
+allAsteroids = sortrows(remainingAsteroids,["angle","dist"])
+
+% allAsteroids = [allAsteroids nan(height(allAsteroids),1)]
 
 
-day10puzzle2result = 0;
+
+
+day10puzzle2result = best %#ok<NOPTS> 
