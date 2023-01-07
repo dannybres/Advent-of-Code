066d3a98697a31755955b7da8269e37c4b9276e3
Daniel Breslan <daniel@breslan.co.uk>
2023-01-07 21:13:27 +0000

    19.14.1 & 19.14.2 done! :)

--- 2019/day 14/day14puzzle1.m
+++ 2019/day 14/day14puzzle1.m
@@ -1,30 +1,43 @@
 %% day14puzzle1 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt").split(" => ")
+day14puzzle1result = calculateOre(1) %#ok<NOPTS> 
 
+function oreNeeded = calculateOre(fuel)
+data = readlines("input.txt").split(" => ");
 [~,e] = regexp(data,"([^0-9 =>,]+)", "tokens","match");
+allEle = unique(horzcat(e{:}))';
+t = table(allEle,zeros(numel(allEle),1),zeros(numel(allEle),1),...
+    VariableNames=["ele","needed","produced"]);
+t.needed(t.ele == "FUEL") = fuel;
 
-all = unique(horzcat(e{:}))'
-
-t = table(all,zeros(numel(all),1),zeros(numel(all),1),VariableNames=["ele","needed","produced"])
-
-t.needed(t.ele == "FUEL") = 1;
-
-toMake = t(t.needed > t.produced,:);
-
-for idx = 1:height(toMake)
-    toMakeRow = toMake(idx,:);
-    needed = toMakeRow.needed - toMakeRow.produced;
-    makes = data(data(:,2).contains(toMakeRow.ele),2).split(" ");
-    makes = makes(1).double;
-    ingredients = data(data(:,2).contains(toMakeRow.ele),1).split(", ");
-
-    reactionsNeeded = ceil(needed/makes);
-
-    for iidx = 1:height(ingredients)
-        thisIngredient = ingredients(iidx,:).split(" ");
-        t.
+while 1
+    toMake = t(t.needed > t.produced,:);
+    toMake = toMake(toMake.ele ~= "ORE",:);
+    for idx = 1:height(toMake)
+        toMakeRow = toMake(idx,:);
+        if toMakeRow.ele == "ORE"
+            if height(toMakeRow) == 1
+                break
+            end
+            continue
+        end
+        needed = toMakeRow.needed - toMakeRow.produced;
+        makes = data(data(:,2).contains(toMakeRow.ele),2).split(" ");
+        makes = makes(1).double;
+        ingredients = data(data(:,2).endsWith(" " + toMakeRow.ele),1)...
+            .split(", ");
+        reactionsNeeded = ceil(needed/makes);
+        for iidx = 1:height(ingredients)
+            thisIngredient = ingredients(iidx,:).split(" ")';
+            t.needed(t.ele == thisIngredient(2)) = t.needed(t.ele ==...
+                thisIngredient(2)) + thisIngredient(1).double *...
+                reactionsNeeded;
+        end
+        t.produced(t.ele == toMakeRow.ele) = t.produced(t.ele == ...
+            toMakeRow.ele) + makes * reactionsNeeded;
+    end
+    if isempty(toMake)
+        break
     end
 end
-
-
-day14puzzle1result = 0;
+oreNeeded = t.needed(t.ele == "ORE");
+end
--- 2019/day 14/day14puzzle2.m
+++ 2019/day 14/day14puzzle2.m
@@ -1,5 +1,50 @@
 %% day14puzzle2 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+needed = 1000000000000;
+oreForOne = calculateOre(1);
+tryOre = needed / oreForOne;
 
+estimtateForTrill = calculateOre(tryOre);
+tryOre = ceil(tryOre * (needed / estimtateForTrill));
 
-day14puzzle2result = 0;
+day14puzzle2result = tryOre %#ok<NOPTS> 
+
+function oreNeeded = calculateOre(fuel)
+data = readlines("input.txt").split(" => ");
+[~,e] = regexp(data,"([^0-9 =>,]+)", "tokens","match");
+allEle = unique(horzcat(e{:}))';
+t = table(allEle,zeros(numel(allEle),1),zeros(numel(allEle),1),...
+    VariableNames=["ele","needed","produced"]);
+t.needed(t.ele == "FUEL") = fuel;
+
+while 1
+    toMake = t(t.needed > t.produced,:);
+    toMake = toMake(toMake.ele ~= "ORE",:);
+    for idx = 1:height(toMake)
+        toMakeRow = toMake(idx,:);
+        if toMakeRow.ele == "ORE"
+            if height(toMakeRow) == 1
+                break
+            end
+            continue
+        end
+        needed = toMakeRow.needed - toMakeRow.produced;
+        makes = data(data(:,2).contains(toMakeRow.ele),2).split(" ");
+        makes = makes(1).double;
+        ingredients = data(data(:,2).endsWith(" " + toMakeRow.ele),1)...
+            .split(", ");
+        reactionsNeeded = ceil(needed/makes);
+        for iidx = 1:height(ingredients)
+            thisIngredient = ingredients(iidx,:).split(" ")';
+            t.needed(t.ele == thisIngredient(2)) = t.needed(t.ele ==...
+                thisIngredient(2)) + thisIngredient(1).double *...
+                reactionsNeeded;
+        end
+        t.produced(t.ele == toMakeRow.ele) = t.produced(t.ele == ...
+            toMakeRow.ele) + makes * reactionsNeeded;
+    end
+    if isempty(toMake)
+        break
+    end
+end
+oreNeeded = t.needed(t.ele == "ORE");
+end
