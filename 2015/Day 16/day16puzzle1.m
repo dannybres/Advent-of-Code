9fd8845db7f369776024acd0dd2ccf2d0eaae804
Daniel Breslan <daniel@breslan.co.uk>
2023-01-12 11:31:31 +0000

    Fixed files

--- 2015/Day 16/day16puzzle1.m
+++ 2015/Day 16/day16puzzle1.m
@@ -1,15 +1,4 @@
 %% day16puzzle1 - Daniel Breslan - Advent Of Code 2015
-data = readlines("input.txt").split("").double;
-data = data(2:end-1)';
-factors = zeros(numel(data),numel(data));
-base = [0 1 0 -1];
-n = 100;
-for phase = 1:n
-    for idx = 1:height(factors)
-        tmp = repmat(repelem(base,1,idx),1,ceil((numel(data)+1) / (4*idx)));
-    	factors(idx,:) = tmp(2:1+numel(data));
-    end
-    data = mod(abs(sum(repmat(data,numel(data),1) .* factors,2)),10)';
-end
+data = readlines("inputDemo.txt")
 
-day16puzzle1result = string(data(1:8)).join("")
+day16puzzle1result = 0
--- 2019/day 16/day16puzzle1.m
+++ 2019/day 16/day16puzzle1.m
@@ -1,5 +1,15 @@
 %% day16puzzle1 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split("").double;
+data = data(2:end-1)';
+factors = zeros(numel(data),numel(data));
+base = [0 1 0 -1];
+n = 100;
+for phase = 1:n
+    for idx = 1:height(factors)
+        tmp = repmat(repelem(base,1,idx),1,ceil((numel(data)+1) / (4*idx)));
+    	factors(idx,:) = tmp(2:1+numel(data));
+    end
+    data = mod(abs(sum(repmat(data,numel(data),1) .* factors,2)),10)';
+end
 
-
-day16puzzle1result = 0;
+day16puzzle1result = string(data(1:8)).join("")
