b19cf75ba01c61db7702ebf461368d33d8f0fc78
Daniel Breslan <daniel@breslan.co.uk>
2023-06-23 15:56:14 +0100

    Unknown

--- 2019/day 19/day19puzzle1.m
+++ 2019/day 19/day19puzzle1.m
@@ -1,5 +1,11 @@
 %% day19puzzle1 - Daniel Breslan - Advent Of Code 2019
-data = readlines("inputDemo.txt")
-
-
+addpath("../intCodeComputer/")
+data = readlines("inputDemo.txt").split(",").double();
+out = nan(50)
+for r = 0:49
+    for c = 0:49
+        out(r+1, c+1) = processIntCodeComputer(data,[r c]);
+    end
+end
+out
 day19puzzle1result = 0;
