18e28bb846d2e311401a5de6a1cb60a8db28d22f
Daniel Breslan <daniel@breslan.co.uk>
2023-06-29 20:44:24 +0100

    Day 8 done! :)

--- 2018/Day 08/day8puzzle1.m
+++ 2018/Day 08/day8puzzle1.m
@@ -1,5 +1,18 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2018
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(" ").double();
 
+[~,day8puzzle1result] = process(data,0) %#ok<NOPTS> 
 
-day8puzzle1result = 0;
+function [data,metaCount] = process(data,metaCount)
+    nCh = data(1); % get num children
+    nMe = data(2); % get num meta
+    data = data(3:end); % dispose of header
+
+    for idx = 1:nCh % process childs recursively
+        [data,metaCount] = process(data,metaCount);
+    end
+
+    meta = sum(data(1:nMe)); % get meta (now children have finished)
+    metaCount = metaCount + meta; % increment count
+    data(1:nMe) = []; % dispose of meta
+end
--- 2018/Day 08/day8puzzle2.m
+++ 2018/Day 08/day8puzzle2.m
@@ -1,5 +1,50 @@
 %% day8puzzle2 - Daniel Breslan - Advent Of Code 2018
-data = readlines("inputDemo.txt")
+clc
+data = readlines("input.txt").split(" ").double();
+
+d = digraph; % diectional graph
+
+[~,d] = process(data,d,"start"); % process it recuresively
+
+% find weight of edge from "start"
+day8puzzle2result = ...
+    d.Edges.Weight(string(d.Edges.EndNodes(:,1)) == "start") 
 
 
-day8puzzle2result = 0;
+function [data,d] = process(data,d,parent) 
+% remaining string, graph, name of parent node
+nodeName = string(char(65 + d.numnodes)); % get string name for node
+d = d.addnode(nodeName); % add node
+d = d.addedge(parent,nodeName,0); % and edge
+
+nCh = data(1); % get num children
+nMe = data(2); % get num meta
+data = data(3:end); % dispose of header
+
+for idx = 1:nCh % process childs recursively
+    [data,d] = process(data,d,nodeName);
+end
+
+meta = data(1:nMe); % get meta (now children have finished)
+data(1:nMe) = []; % dispose of meta
+
+if nCh == 0 % if no children set weigh = to sum of meta, need to lookup 
+    % edge of parent to nodeNAme
+    d.Edges.Weight(string(d.Edges.EndNodes).join() == ...
+        join([parent nodeName])) = sum(meta);
+else
+    % if childrem loop meta and add the value of children (if exist)
+    allEdges = string(d.Edges.EndNodes);
+    edgesFromHere = d.Edges(allEdges(:,1) == nodeName,:);
+    nodeValue = 0;
+    for midx = meta'
+        if midx > height(edgesFromHere)
+            continue
+        else
+            nodeValue = nodeValue + edgesFromHere.Weight(midx);
+        end
+    end
+    d.Edges.Weight(string(d.Edges.EndNodes).join() == ...
+        join([parent nodeName])) = nodeValue;
+end
+end
