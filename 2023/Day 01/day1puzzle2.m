84c8feab79d610aff3b9c071052f597867829da6
Daniel Breslan <daniel@breslan.co.uk>
2023-12-01 15:04:24 +0000

    day 1 updated

--- 2023/Day 01/day1puzzle2.m
+++ 2023/Day 01/day1puzzle2.m
@@ -1,23 +1,10 @@
 %% day1puzzle2 - Daniel Breslan - Advent Of Code 2023
 data = readlines("input.txt");
-day1puzzle1result = sum(arrayfun(@getNums,data)) %#ok<NOPTS> 
-
-function out = getNums(data)
-ogdata = data;
-idx =sort([regexp(data, "one") regexp(data, "two") regexp(data, "three")...
-    regexp(data, "four") regexp(data, "five") regexp(data, "six") ...
-    regexp(data, "seven") regexp(data, "eight") regexp(data, "nine") ...
-    regexp(data,string(1:9).join("|"))]);
-idx = idx([1 end]);
-start = repmat(data,1,2).extract(idx);
-areNum = ~cellfun(@isempty,regexp(start,"\d"));
-nums = repmat("",1,2);
-nums(areNum) = start(areNum);
-for nidx = find(nums == "")
-    nums(nidx) = ogdata.extractBetween(idx(nidx),idx(nidx)+2).replace(...
-        ["one", "two", "thr", "fou", "fiv", "six", "sev", "eig", "nin", ...
-        "ten"],...
-        string(1:10));
+for idx = 1:2
+    data = data.replace(["one","two","three","four","five", ...
+        "six","seven","eight","nine"],...
+        ["o1e", "t2o", "t3e", "4", "5e", "6", "7n", "e8t", "9e"]);
 end
-out = nums.join("").double;
-end
+nums = regexp(data,"\d",'match');
+fun = @(x) x([1 end]).join("").double;
+day1puzzle1result = sum(cell2mat(cellfun(fun,nums,UniformOutput=false))) %#ok<NOPTS> 
