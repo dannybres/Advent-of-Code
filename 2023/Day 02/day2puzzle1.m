f8d0ebf89b811b5fa2a443d14db3fda210c1f1ca
Daniel Breslan <daniel@breslan.co.uk>
2023-12-02 09:38:46 +0000

    2023 Day 2 done

--- 2023/Day 02/day2puzzle1.m
+++ 2023/Day 02/day2puzzle1.m
@@ -1,5 +1,11 @@
 %% day2puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = readlines("input.txt");
+day2puzzle1result = sum(find(arrayfun(@gamePossible,data)))
 
-
-day2puzzle1result = 0;
+function out = gamePossible(in)
+    game = in.split([": ",", ","; "]).split(" ");
+    ball = game(2:end,2);
+    count = game(2:end,1).double;
+    out = ~any(count - (ball.extractBefore(2) == ["r","g","b"]) ...
+        * (0:2)' > 12);
+end
--- 2023/Day 02/day2puzzle2.m
+++ 2023/Day 02/day2puzzle2.m
@@ -1,5 +1,9 @@
 %% day2puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = readlines("input.txt");
+day2puzzle1result = sum(arrayfun(@gamePower,data)) %#ok<NOPTS> 
 
-
-day2puzzle2result = 0;
+function out = gamePower(in)
+    game = in.split([": ",", ","; "]).split(" ");
+    out = prod(max((game(2:end,2).extractBefore(2) == ["r","g","b"])' ...
+        .* repmat(game(2:end,1).double',3,1),[],2));
+end
