bf28f4604c705512f2c0efd36977a1dd361ab166
Daniel Breslan <danielbreslan@Daniels-MacBook-Air.local>
2023-12-04 22:55:37 +0000

    2023 Day 4.2 updated

--- 2023/Day 04/day4puzzle2.m
+++ 2023/Day 04/day4puzzle2.m
@@ -1,21 +1,12 @@
 %% day4puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("input.txt").split([":" "|"]);
+data = readlines("input.txt");
+winners = data.extractBetween(":","|").extract(digitsPattern).double;
+my = data.extractAfter("|").extract(digitsPattern).double;
  
-data = data(:,2:end).replace("  "," ").replace("  "," ")...
-    .replace("  "," ");
- 
-winners = data(:,1).split(" ");
-winners = winners(:,~all(winners == "")).double;
-my = data(:,2).split(" ");
-my = my(:,~all(my == "")).double;
- 
-r = nan(height(winners),1);
 n = ones(height(winners),1);
 for idx = 1:height(winners)
-    r(idx) = sum(winners(idx,:) == my(idx,:)',"all");
-    if r(idx) > 0
-    n(idx+1:idx+r(idx)) = n(idx+1:idx+r(idx)) + n(idx);
-    end
+    r = sum(winners(idx,:) == my(idx,:)',"all");
+    n(idx+1:idx+r) = n(idx+1:idx+r) + n(idx);
 end
 
 day4puzzle2result = sum(n) %#ok<NOPTS>
