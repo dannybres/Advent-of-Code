ab5e371a2b6f8d956b52c65e063e4a7057273a8a
Daniel Breslan <danielbreslan@Daniels-MacBook-Air.local>
2023-12-04 21:59:47 +0000

    2023 Day 4 done, needs day 2 improving

--- 2023/Day 04/day4puzzle1.m
+++ 2023/Day 04/day4puzzle1.m
@@ -1,5 +1,8 @@
 %% day4puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+% Thanks eMoss - https://discord.com/channels/315631984489791488/452325884955852800/1181343396577677373
+data = readlines("input.txt");
+win = data.extractBetween(":","|").extract(digitsPattern).double;
+game = data.extractAfter("|").extract(digitsPattern).double;
+numMatches = sum(reshape(win,height(win),1,[]) == game,[2 3]);
 
-
-day4puzzle1result = 0;
+day4puzzle1result = sum(floor(2.^(numMatches-1))) %#ok<NOPTS>
--- 2023/Day 04/day4puzzle2.m
+++ 2023/Day 04/day4puzzle2.m
@@ -1,5 +1,21 @@
 %% day4puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split([":" "|"]);
+ 
+data = data(:,2:end).replace("  "," ").replace("  "," ")...
+    .replace("  "," ");
+ 
+winners = data(:,1).split(" ");
+winners = winners(:,~all(winners == "")).double;
+my = data(:,2).split(" ");
+my = my(:,~all(my == "")).double;
+ 
+r = nan(height(winners),1);
+n = ones(height(winners),1);
+for idx = 1:height(winners)
+    r(idx) = sum(winners(idx,:) == my(idx,:)',"all");
+    if r(idx) > 0
+    n(idx+1:idx+r(idx)) = n(idx+1:idx+r(idx)) + n(idx);
+    end
+end
 
-
-day4puzzle2result = 0;
+day4puzzle2result = sum(n) %#ok<NOPTS>
