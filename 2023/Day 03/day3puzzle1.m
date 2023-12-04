f9dff7968b95fb6d4999afd391fc80689920f2c3
Daniel Breslan <danielbreslan@Daniels-MacBook-Air.local>
2023-12-04 22:31:32 +0000

    2023 Day 3 done

--- 2023/Day 03/day3puzzle1.m
+++ 2023/Day 03/day3puzzle1.m
@@ -1,5 +1,34 @@
 %% day3puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = char(readlines("input.txt"));
+allChars = unique(data(:));
+allChars = double(allChars);
+allChars = allChars(allChars ~= double('.'));
 
+symbols = any(data == permute( ...
+    char(allChars(allChars < double('0') | allChars > double('9'))), ...
+    [3 2 1]),3);
+numbers = any(data == permute(char(double('0'):double('9'))', ...
+    [3 2 1]),3);
 
-day3puzzle1result = 0;
+c = ones(3);
+c(5) = 0;
+symbolNeighbour = conv2(symbols,c,'same');
+res = repmat(' ',size(data));
+toProcess = symbolNeighbour & numbers;
+for idx = 1:height(toProcess)
+    res(idx,toProcess(idx,:)) = data(idx,toProcess(idx,:));
+    for x = 1:3
+        tp = padarray(res(idx,:) ~= ' ',[0 1]);
+        n = padarray(numbers(idx,:),[0 1]);
+        for cidx = 2:width(tp)-1
+            if(any(tp(cidx + [-1 1])) && n(cidx))
+                res(idx,cidx-1) = data(idx,cidx-1);
+            end
+        end
+    end
+end
+res = [res repmat(' ',height(res),1)];
+res = res';
+res = string(res(:)');
+res = res.split(" ");
+day3puzzle1result = sum(res(res~="").double) %#ok<NOPTS>
--- 2023/Day 03/day3puzzle2.m
+++ 2023/Day 03/day3puzzle2.m
@@ -1,5 +1,55 @@
 %% day3puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day3puzzle2result = 0;
+clc
+data = char(readlines("input.txt"));
+data = char(padarray(double(data),[1 1],double('.')));
+allChars = unique(data(:));
+allChars = double(allChars);
+allChars = allChars(allChars ~= double('.'));
+numbers = any(data == permute(char(double('0'):double('9'))', ...
+    [3 2 1]),3);
+gears = find(data == '*');
+n = nan(numel(gears),1);
+res = 0;
+for idx = 1:numel(gears)
+    [r,c]=ind2sub(size(data),gears(idx));
+    isn = false(5,3);
+    isn(1:2:end,:) = numbers(r+(-1:1), c+(-1:1));
+    o = bwconncomp(isn);
+    n(idx) = o.NumObjects;
+    if o.NumObjects == 2
+        isn = false(5,3);
+        for cidx = 1:2
+            isn(o.PixelIdxList{cidx}(1)) = 1;
+        end
+        isn = isn(1:2:end,:);
+        [ri,ci] = ind2sub([3 3],find(isn));
+        ri = ri + r - 2;
+        ci = ci + c - 2;
+        n1 = data(ri(1),ci(1));
+        n2 = data(ri(2),ci(2));
+        sh = ones(1,4);
+        for idx = 1:2
+            if any(data(ri(1),ci(1)-sh(1)) == char(double('0'):double('9')))
+                n1 = [data(ri(1),ci(1)-sh(1)) n1];
+                sh(1) = sh(1) + 1;
+            end
+            if any(data(ri(1),ci(1)+sh(2)) == char(double('0'):double('9')))
+                n1 = [n1 data(ri(1),ci(1)+sh(2))];
+                sh(2) = sh(2) + 1;
+            end
+            if any(data(ri(2),ci(2)-sh(3)) == char(double('0'):double('9')))
+                n2 = [data(ri(2),ci(2)-sh(3)) n2];
+                sh(3) = sh(3) + 1;
+            end
+            if any(data(ri(2),ci(2)+sh(4)) == char(double('0'):double('9')))
+                n2 = [n2 data(ri(2),ci(2)+sh(4))];
+                sh(4) = sh(4) + 1;
+            end
+        end
+    res = res + prod([string(n1).double,string(n2).double]);
+    if isnan(res)
+        % disp(1)
+    end
+    end
+end
+day3puzzle2result = res %#ok<NOPTS>
