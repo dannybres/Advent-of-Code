83c70c2d4583dd90d7ffe8f47700af82b8ca61f4
Daniel Breslan <danielbreslan@Daniels-MacBook-Air.local>
2023-12-05 23:18:41 +0000

    2023 Day 5 done

--- 2023/Day 05/day5puzzle1.m
+++ 2023/Day 05/day5puzzle1.m
@@ -1,5 +1,16 @@
 %% day5puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
+data = readlines("inputDemo.txt");
+seeds = data(1).extract(digitsPattern).double;
+data = data(3:end);
+points = [[1;find(data == "")+1],[find(data == "")-1;numel(data)]];
+loc = seeds;
+for idx = 1:height(points)
+    m = data(points(idx,1)+1:points(idx,2)).extract(digitsPattern).double;
+    m(:,4) = m(:,2) + m(:,3) - 1;
+    shift = diff(m(:,2:-1:1),[],2);
+    tf = loc>=m(:,2)' & loc <=m(:,4)';
+    loc = loc + tf * shift;
+end
+min(loc)
 
 day5puzzle1result = 0;
--- 2023/Day 05/day5puzzle2.m
+++ 2023/Day 05/day5puzzle2.m
@@ -1,5 +1,57 @@
 %% day5puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = readlines("inputDemo.txt");
+ranges = reshape(data(1).extract(digitsPattern).double,2,[])';
+ranges(:,2) = sum(ranges,2) - 1;
+% ranges = [79 79;14 14;55 55;14 14]
+data = data(3:end);
+levelPoints = [[1;find(data == "")+1],[find(data == "")-1;numel(data)]];
+for idx = 1:height(levelPoints)
+    % disp(compose("Level %i, with %i ranges", idx, height(ranges)))
+    d = data(levelPoints(idx,1)+1:levelPoints(idx,2)).extract(digitsPattern).double;
+    map = [d(:,2) d(:,2) + d(:,3) - 1, diff(d(:,2:-1:1),[],2)];
+    [~,srtIdx] = sort(map(:,1));
+    map = map(srtIdx,:);
+    ranges = splitRanges(map, ranges);
+    ranges = translateToNextMap(ranges, map);
+end
+day5puzzle2result = min(ranges,[],"all")
 
+function rr = splitRanges(map, ranges)
+rr = [];
+for idx = 1:height(ranges)
+    tempRan = nan(height(map),2);
+    rtp = ranges(idx,:);
+    tempRan(map(:,1:2) > rtp(1) & map(:,1:2) < rtp(2)) = map(map(:,1:2) > rtp(1) & map(:,1:2) < rtp(2));
+    tempRan(all(isnan(tempRan),2),:) = [];
+    if ~isempty(tempRan)
+        tempRan = [rtp(1), nan;...
+            tempRan;...
+            nan, rtp(2)];
+        tempRan = tempRan';
+        tempRan = tempRan(:);
+        trin = isnan(tempRan);
+        for tidx = numel(trin)-1:-2:2
+            if all(trin(tidx-1:tidx))
+                tempRan(tidx-1:tidx) = [];
+            end
+        end
+        tempRan = reshape(tempRan,2,[])';
+        c1i = find(isnan(tempRan(:,1)));
+        tempRan(c1i,1) = tempRan(c1i-1,2) + 1;
+        c2i = find(isnan(tempRan(:,2)));
+        tempRan(c2i,2) = tempRan(c2i+1,1) - 1;
+        rr = [rr;tempRan];
+    else
+        rr = [rr;rtp]; %#ok<*AGROW>
+    end
+end
+end
 
-day5puzzle2result = 0;
+function ranges = translateToNextMap(ranges, map)
+ranges = ranges(:);
+tf = ranges>=map(:,1)' & ranges <=map(:,2)';
+ranges = ranges + tf * map(:,3);
+ranges = reshape(ranges,[],2);
+[~,srtIdx] = sort(ranges(:,1));
+ranges = ranges(srtIdx,:)
+end
