ea732d050582ab54f0b7ff9beb4f26949fe8fc9f
Daniel Breslan <daniel@breslan.co.uk>
2023-12-06 09:23:54 +0000

    2023 Day 6 done

--- 2023/Day 06/day6puzzle1.m
+++ 2023/Day 06/day6puzzle1.m
@@ -1,5 +1,8 @@
 %% day6puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day6puzzle1result = 0;
+data = readlines("input.txt").extract(digitsPattern).double();
+res = 1;
+for idx = 1:size(data,2)
+    res = prod([res, sum((0:data(1,idx)) .* (data(1,idx) ...
+        - (0:data(1,idx))) > data(2,idx))]);
+end
+day6puzzle1result = res %#ok<NOPTS> 
--- 2023/Day 06/day6puzzle2.m
+++ 2023/Day 06/day6puzzle2.m
@@ -1,5 +1,3 @@
 %% day6puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day6puzzle2result = 0;
+data = readlines("input.txt").extract(digitsPattern).join("").double();
+day6puzzle2result = sum((0:data(1,idx)) .* (data(1,idx) - (0:data(1,idx))) > data(2,idx)) %#ok<NOPTS> 
