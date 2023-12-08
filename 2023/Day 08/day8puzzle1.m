cb41450c268c9f87b63e7f7748abc98f9d953875
Daniel Breslan <daniel@breslan.co.uk>
2023-12-08 11:22:34 +0000

    2023 Day 8 done

--- 2023/Day 08/day8puzzle1.m
+++ 2023/Day 08/day8puzzle1.m
@@ -1,5 +1,22 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = readlines("input.txt");
+d = data(1); d = d.split(""); d = d(2:end-1);
+m = dictionary(reshape(data(3:end).extractBefore(" ")'+["L";"R"],[],1), ...
+    reshape(data(3:end).extractBetween("(",")").split(", ")',[],1));
 
+loc = "AAA";
+idx = 1;
+while loc ~= "ZZZ"
+    loc = m(loc+getDir(idx,d));
+    idx = idx + 1;
+end
 
-day8puzzle1result = 0;
+day8puzzle1result = idx -1
+
+function dir = getDir(n,d)
+    n = mod(n,numel(d));
+    if  n == 0
+        n = numel(d);
+    end
+    dir = d(n);
+end
--- 2023/Day 08/day8puzzle2.m
+++ 2023/Day 08/day8puzzle2.m
@@ -1,5 +1,34 @@
-%% day8puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+%% day8puzzle1 - Daniel Breslan - Advent Of Code 2023
+clc
+data = readlines("input.txt");
+d = data(1); d = d.split(""); d = d(2:end-1);
+m = dictionary(reshape(data(3:end).extractBefore(" ")'+["L";"R"],[],1), ...
+    reshape(data(3:end).extractBetween("(",")").split(", ")',[],1));
+
+locs = data(3:end).extractBefore(" ");
+locs = locs(locs.extract(3) == "A");
+zs = cell(1,numel(locs));
+
+idx = 1;
+while any(cellfun(@isempty,zs))
+    locs = m(locs+getDir(idx,d));
+    if any(locs.extract(3) == "Z")
+        zidxs = find(locs.extract(3) == "Z");
+        for zidx = zidxs'
+            zs{zidx} = [zs{zidx} idx];
+        end
+    end
+    idx = idx + 1;
+end
+q = cell2mat(cellfun(@(x) x(1),zs,'UniformOutput',false)');
+format long
+day8puzzle2result = lcm(lcm(lcm(lcm(lcm(q(1),q(2)),q(3)),q(4)),q(5)),q(6)) %#ok<NOPTS> 
 
 
-day8puzzle2result = 0;
+function dir = getDir(n,d)
+    n = mod(n,numel(d));
+    if  n == 0
+        n = numel(d);
+    end
+    dir = d(n);
+end
