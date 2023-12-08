36e090f8c7998cc7de7fa2e99b64dd2e3148ad22
Daniel Breslan <daniel@breslan.co.uk>
2023-12-08 12:19:07 +0000

    2023 Day 8 optimised, no cells, no mode, repmat directions

--- 2023/Day 08/day8puzzle2.m
+++ 2023/Day 08/day8puzzle2.m
@@ -1,34 +1,20 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2023
-clc
 data = readlines("input.txt");
 d = data(1); d = d.split(""); d = d(2:end-1);
+d = repmat(d,100,1);
 m = dictionary(reshape(data(3:end).extractBefore(" ")'+["L";"R"],[],1), ...
     reshape(data(3:end).extractBetween("(",")").split(", ")',[],1));
 
 locs = data(3:end).extractBefore(" ");
 locs = locs(locs.extract(3) == "A");
-zs = cell(1,numel(locs));
+
+zs = nan(1,numel(locs));
 
 idx = 1;
-while any(cellfun(@isempty,zs))
-    locs = m(locs+getDir(idx,d));
-    if any(locs.extract(3) == "Z")
-        zidxs = find(locs.extract(3) == "Z");
-        for zidx = zidxs'
-            zs{zidx} = [zs{zidx} idx];
-        end
-    end
+while any(isnan(zs))
+    locs = m(locs+d(idx));
+    zs(locs.extract(3) == "Z") = idx;
     idx = idx + 1;
 end
-q = cell2mat(cellfun(@(x) x(1),zs,'UniformOutput',false)');
 format long
-day8puzzle2result = lcm(lcm(lcm(lcm(lcm(q(1),q(2)),q(3)),q(4)),q(5)),q(6)) %#ok<NOPTS> 
-
-
-function dir = getDir(n,d)
-    n = mod(n,numel(d));
-    if  n == 0
-        n = numel(d);
-    end
-    dir = d(n);
-end
+day8puzzle2result = lcm(lcm(lcm(lcm(lcm(zs(1),zs(2)),zs(3)),zs(4)),zs(5)),zs(6)) %#ok<NOPTS> 
