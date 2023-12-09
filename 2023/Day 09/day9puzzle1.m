42815f17c188a5a9eecef4a5549e52bf56c53e9e
Daniel Breslan <daniel@breslan.co.uk>
2023-12-09 23:02:56 +0000

    2023 Day 9 done.

--- 2023/Day 09/day9puzzle1.m
+++ 2023/Day 09/day9puzzle1.m
@@ -1,5 +1,8 @@
 %% day9puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day9puzzle1result = 0;
+data = readlines("input.txt").split(" ").double;
+summer = 0;
+for idx = 1:height(data)
+    p = polyfit(1:width(data),data(idx,:),width(data)-1);
+    summer = summer + round(polyval(p,width(data)+1));
+end
+day9puzzle1result = summer %#ok<NOPTS> 
--- 2023/Day 09/day9puzzle2.m
+++ 2023/Day 09/day9puzzle2.m
@@ -1,5 +1,8 @@
 %% day9puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day9puzzle2result = 0;
+data = readlines("input.txt").split(" ").double;
+summer = 0;
+for idx = 1:height(data)
+    summer = summer + round(polyval( ...
+        polyfit(1:width(data),data(idx,:),width(data)-1),0));
+end
+day9puzzle2result = summer %#ok<NOPTS> 
