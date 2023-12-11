b4f0acadd0feda8377c99ba764eb126c725900f5
Daniel Breslan <danielbreslan@Daniels-Air.broadband>
2023-12-11 12:29:11 +0000

    2023 Day 11 updated with some more efficent and les complex code

--- 2023/Day 11/day11puzzle1.m
+++ 2023/Day 11/day11puzzle1.m
@@ -1,9 +1,9 @@
 %% day11puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = char(readlines("input.txt"));
+data = char(readlines("inputDemo.txt"));
 [r,c] = ind2sub(size(data),find(data == '#'));
 blanks = data == '.';
 cs = cumsum(ones(1,size(data,1)) + all(blanks));
 rs = cumsum(ones(size(data,2),1) + all(blanks,2));
 r = rs(r);
 c = cs(c)';
-day11puzzle1result = (sum(abs(c - c'),"all") + sum(abs(r - r'),"all"))/2 %#ok<NOPTS>
+day11puzzle1result = (sum(abs(c - c') + abs(r - r'),"all"))/2 %#ok<NOPTS>
--- 2023/Day 11/day11puzzle2.m
+++ 2023/Day 11/day11puzzle2.m
@@ -2,8 +2,8 @@
 data = char(readlines("input.txt"));
 [r,c] = ind2sub(size(data),find(data == '#'));
 blanks = data == '.';
-cs = cumsum(ones(1,size(data,1)) + max(0,all(blanks)*1e6-1));
-rs = cumsum(ones(size(data,1),1) + max(0,all(blanks,2)*1e6-1));
+cs = cumsum(ones(1,size(data,1)) + all(blanks)*(1e6-1));
+rs = cumsum(ones(size(data,2),1) + all(blanks,2)*(1e6-1));
 r = rs(r);
 c = cs(c)';
-day11puzzle2result = (sum(abs(c - c'),"all") + sum(abs(r - r'),"all"))/2 %#ok<NOPTS>
+day11puzzle2result = (sum(abs(c - c') + abs(r - r'),"all"))/2 %#ok<NOPTS>
