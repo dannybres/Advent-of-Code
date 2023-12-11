a64808d3fc5e1b38619f1a1bd2c5a32ae09fd835
Daniel Breslan <danielbreslan@Daniels-Air.broadband>
2023-12-11 12:10:03 +0000

    2023 Day 11 done

--- 2023/Day 11/day11puzzle1.m
+++ 2023/Day 11/day11puzzle1.m
@@ -1,5 +1,9 @@
 %% day11puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day11puzzle1result = 0;
+data = char(readlines("input.txt"));
+[r,c] = ind2sub(size(data),find(data == '#'));
+blanks = data == '.';
+cs = cumsum(ones(1,size(data,1)) + all(blanks));
+rs = cumsum(ones(size(data,2),1) + all(blanks,2));
+r = rs(r);
+c = cs(c)';
+day11puzzle1result = (sum(abs(c - c'),"all") + sum(abs(r - r'),"all"))/2 %#ok<NOPTS>
