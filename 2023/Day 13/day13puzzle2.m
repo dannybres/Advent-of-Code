5a80bf68b86dba0ccb2dfe81cb9427917f5b4ebf
Daniel Breslan <daniel@breslan.co.uk>
2023-12-13 09:46:34 +0000

    2023 Day 13 done, much easier

--- 2023/Day 13/day13puzzle2.m
+++ 2023/Day 13/day13puzzle2.m
@@ -1,5 +1,30 @@
 %% day13puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+data = [""; readlines("input.txt"); ""];
+splits = find(data == "");
+
+r = nan(numel(splits)-1,2);
+for idx = 1:numel(splits)-1
+    m = char(data(splits(idx)+1:splits(idx+1)-1));
+    r(idx,:) = [0 processMap(m)];
+    if isnan(r(idx,2))
+        r(idx,:) = [1 processMap(m')];
+    end
+end
+r(r(:,1) == 1,2) = r(r(:,1) == 1,2) * 100;
+day13puzzle2result = sum(r(:,2)) %#ok<NOPTS>
 
 
-day13puzzle2result = 0;
+function aidx = processMap(m)
+w = width(m);
+afterLine = 1:w-1;
+for aidx = 1:numel(afterLine)
+    a = afterLine(aidx);
+    widthOfThem = min(a,w-a);
+    lhs = m(:,a-widthOfThem+1:a);
+    rhs = fliplr(m(:,a+1:a+widthOfThem));
+    if sum((lhs == rhs) == false,'all') == 1
+        return
+    end
+end
+aidx = nan;
+end
