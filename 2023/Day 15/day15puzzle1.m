812a77353054bea4d9932009d73ec0e09220eeca
Daniel Breslan <daniel@breslan.co.uk>
2023-12-15 12:58:51 +0000

    2023 Day 15 done

--- 2023/Day 15/day15puzzle1.m
+++ 2023/Day 15/day15puzzle1.m
@@ -1,5 +1,16 @@
 %% day15puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day15puzzle1result = 0;
+% Determine the ASCII code for the current character of the string.
+% Increase the current value by the ASCII code you just determined.
+% Set the current value to itself multiplied by 17.
+% Set the current value to the remainder of dividing itself by 256.
+data = readlines("input.txt").split(",");
+r = nan(numel(data),1);
+for idx = 1:numel(data)
+h = 0;
+s = char(data(idx));
+for c = s
+    h = mod((h + double(c))*17,256);
+end
+r(idx) = h;
+end
+day15puzzle1result = sum(r)
--- 2023/Day 15/day15puzzle2.m
+++ 2023/Day 15/day15puzzle2.m
@@ -1,5 +1,37 @@
 %% day15puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+clc
+data = readlines("input.txt").split(",");
+l = repmat("",9,255);
+for idx = 1:numel(data)
+    instr = data(idx);
+    label = instr.extractBefore("-");
+    if ismissing(label)
+        label = instr.extractBefore("=");
+    end
+    box = hash(label) + 1;
+    if instr.contains("=")
+        existIdx = l(:,box).contains(label);
+        if any(existIdx)
+            l(existIdx,box) = instr.replace("="," ");
+        else
+            l(find(l(:,box) == "",1),box) = instr.replace("="," ");
+        end
+    else
+        ll = find(l(:,box).contains(label));
+        if ~isempty(ll)
+            r = [l(:,box);""];
+            r(ll) = [];
+            l(:,box) = r;
+        end
+    end
+end
+[slotN,boxN] = find(l~="");
+day15puzzle2result = sum(slotN .* boxN .* l(l~="").extractAfter(" ").double)
 
-
-day15puzzle2result = 0;
+function h = hash(i)
+h = 0;
+s = char(i);
+for c = s
+    h = mod((h + double(c))*17,256);
+end
+end
