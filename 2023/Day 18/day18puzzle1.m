b0ab288da27493af4862ea2c3276272580181ecf
danielbreslan <danielbreslan@localhost>
2023-12-18 21:57:34 +0000

    2023 Day 18 complete

--- 2023/Day 18/day18puzzle1.m
+++ 2023/Day 18/day18puzzle1.m
@@ -1,5 +1,12 @@
 %% day18puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day18puzzle1result = 0;
+data = readlines("input.txt").split(" ");
+d = data(:,1);
+l = data(:,2).double;
+lu = dictionary(["U";"D";"L";"R"],{[-1 0];[1 0];[0 -1];[0 1]});
+c = zeros(numel(l)+1,2);
+for idx = 2:height(c)
+    c(idx,:) = c(idx-1,:) + (lu{d(idx-1)} * l(idx-1));
+end
+% close all
+% plot(c(:,1),c(:,2))
+day18puzzle1result = polyarea(c(:,1),c(:,2)) + sum(l)/2 +1 %#ok<NOPTS>
--- 2023/Day 18/day18puzzle2.m
+++ 2023/Day 18/day18puzzle2.m
@@ -1,5 +1,10 @@
 %% day18puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day18puzzle2result = 0;
+data = readlines("input.txt").split(" ");
+l = hex2dec(data(:,3).extractBetween(3,7));
+d = data(:,3).extract(8).double + 1;
+lu = [0 1;1 0;0 -1;-1 0];
+c = zeros(numel(l)+1,2);
+for idx = 2:height(c)
+    c(idx,:) = c(idx-1,:) + (lu(d(idx-1),:) * l(idx-1));
+end
+day18puzzle2result = polyarea(c(:,1),c(:,2)) + sum(l)/2 +1 %#ok<NOPTS>
