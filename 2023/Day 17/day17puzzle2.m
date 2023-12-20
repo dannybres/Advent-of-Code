86d9039d3038e44a47d1724f92688e19bcfe5873
Daniel Breslan <daniel@breslan.co.uk>
2023-12-20 11:23:58 +0000

    Implemented with a MATLAB authored priority queue

--- 2023/Day 17/day17puzzle2.m
+++ 2023/Day 17/day17puzzle2.m
@@ -4,20 +4,21 @@ map = readlines("input.txt").split("").double;
 map = map(:,2:end-1);
 maxN = 10;
 seen = false(size(map,1),size(map,2),3,3,maxN);
-q = nan(1e6,6);
-q(1,:) = [0 1 1 0 0 0];
-next = 2;
+q = PQ2(12e3);
+q.push([0 1 1 0 0 0],0);
+% next = 2;
 while true
-    o = q(1,:);
-    next = find(isnan(q(1:next+1,1)),1);
-    q(1,:) = nan;
+    o = q.pop;
+    if rand() < 0.005
+        o(1)
+    end
+
     hl = o(1); r = o(2); c = o(3); dr = o(4); dc = o(5); n = o(6);
     if r == size(map,1) && c == size(map,2) && n >= 4 % found end
         break
     end
     if n ~= 0
         if seen(r,c,dr+2,dr+2,n) % seen before (from that direction)
-            q(1:next-1,:) = sortrows(q(1:next-1,:),1);
             continue
         end
         seen(r,c,dr+2,dr+2,n) = true;
@@ -26,8 +27,8 @@ while true
         nr = r + dr;
         nc = c + dc;
         if nr <= size(map,1) && nr > 0 && nc <= size(map,2) && nc > 0
-            q(next,:) = [hl+map(nr,nc) nr nc dr dc n+1];
-            next = next + 1;
+            nhl = hl+map(nr,nc);
+            q.push([nhl nr nc dr dc n+1],nhl);
         end
     end
     if n >= 4 || n == 0
@@ -36,13 +37,12 @@ while true
                 nr = r + idx(1);
                 nc = c + idx(2);
                 if nr <= size(map,1) && nr > 0 && nc <= size(map,2) && nc > 0
-                    q(next,:) = [hl+map(nr,nc) nr nc idx(1) idx(2) 1];
-                    next = next + 1;
+                    nhl = hl+map(nr,nc);
+                    q.push([nhl nr nc idx(1) idx(2) 1],nhl);
                 end
             end
         end
     end
-    q(1:next-1,:) = sortrows(q(1:next-1,:),1);
 end
 day17puzzle2result = hl %#ok<NOPTS>
 
