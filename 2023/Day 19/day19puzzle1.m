8b3223d7487fc1b6155b4d726c5217f2f09e1045
Daniel Breslan <daniel@breslan.co.uk>
2023-12-20 11:24:24 +0000

    2023 Day 19 done.

--- 2023/Day 19/day19puzzle1.m
+++ 2023/Day 19/day19puzzle1.m
@@ -1,5 +1,43 @@
 %% day19puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+clc
+data = readlines("input.txt");
+m = data(1:find(data == "")-1);
+m = m.extractBefore("}").split("{");
+m = dictionary(m(:,1),m(:,2));
+p = data((find(data == "")+1):end);
 
+r = false(numel(p),1);
+for idx = 1:numel(p)
+    cp = p(idx).extractBetween("{","}").split([",","="]);
+    d = dictionary(cp(1:2:end),cp(2:2:end).double);
+    r(idx) = "A" == runMachine(m,d);
+end
 
-day19puzzle1result = 0;
+day19puzzle1result = ...
+    sum(p(r).extract(digitsPattern).double(),'all') %#ok<NOPTS>
+
+function nextWorkflow = runMachine(machine,ratings)
+nextWorkflow = "in";
+while ~(nextWorkflow == "A" || nextWorkflow == "R")
+    ins = machine(nextWorkflow).split(",");
+    for idx = 1:numel(ins)
+        i = ins(idx);
+        if i.contains("<")
+            i = i.split(["<",":"]);
+            if ratings(i(1)) < i(2).double
+                nextWorkflow = i(3);
+                break
+            end
+        elseif i.contains(">")
+            i = i.split([">",":"]);
+            if ratings(i(1)) > i(2).double
+                nextWorkflow = i(3);
+                break
+            end
+        else
+            nextWorkflow = i;
+        end
+    end
+end
+end
+
--- 2023/Day 19/day19puzzle2.m
+++ 2023/Day 19/day19puzzle2.m
@@ -1,5 +1,59 @@
-%% day19puzzle2 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
+%% day19puzzle1 - Daniel Breslan - Advent Of Code 2023
+clc
+data = readlines("input.txt");
+m = data(1:find(data == "")-1);
+m = m.extractBefore("}").split("{");
+rules = cell(height(m),1);
+m = dictionary(m(:,1),m(:,2));
+t = array2table(repmat([1 4e3],4,1),...
+    RowNames=["x","m","a","s"],...
+    VariableNames=["min","max"]);
 
+day19puzzle2result = count(m,t) %#ok<NOPTS>
 
-day19puzzle2result = 0;
+function total = count(workflows,ranges,name)
+    if nargin < 3
+        name = "in";
+    end
+
+    if name == "R"
+        total = 0;
+        return
+    elseif name == "A"
+        total = prod(diff(ranges.Variables,1,2)+1);
+        return
+    end
+    workflow = workflows(name).split(",");
+
+    total = 0;
+    doFallBack = true;
+    for idx = 1:numel(workflow) - 1
+        w = workflow(idx).split(":");
+        key = w(1).extract(1);
+        cmp = w(1).extract(2);
+        value = w(1).extractAfter(2).double;
+        destination = w(2);
+        range = ranges{key,:};
+        if cmp == "<"
+            T = [range(1) min(value-1, range(2))];
+            F = [max(value,range(1)) range(2)];
+        else
+            T = [max(value+1, range(1)) range(2)];
+            F = [range(1) min(value, range(2))];
+        end
+        if T(1) <= T(2)
+            copy = ranges;
+            copy{key,:} = T;
+            total = total + count(workflows,copy,destination);
+        end
+        if F(1) <= F(2)
+            ranges{key,:} = F;
+        else
+            doFallBack = false;
+            break
+        end
+    end
+    if doFallBack
+        total = total + count(workflows,ranges,workflow(end));
+    end
+end
