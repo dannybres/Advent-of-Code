f279372f675f0f19e5382966ef1e8f225d585838
danielbreslan <danielbreslan@192.168.0.146>
2023-12-25 21:11:37 +0000

    day 25 shortcut

--- 2023/Day 25/day25puzzle1.m
+++ 2023/Day 25/day25puzzle1.m
@@ -1,5 +1,16 @@
 %% day25puzzle1 - Daniel Breslan - Advent Of Code 2023
-data = readlines("inputDemo.txt")
-
-
-day25puzzle1result = 0;
+data = readlines("input.txt");
+d = graph;
+for idx = 1:numel(data)
+    node = data(idx).extractBefore(":");
+    for to = data(idx).extractAfter(": ").split(" ")'
+        d = addedge(d,node,to);
+    end
+end
+plot(d)
+d = rmedge(d,'zns','jff');
+d = rmedge(d,'nvb','fts');
+d = rmedge(d,'kzx','qmr');
+plot(d)
+c = conncomp(d);
+day25puzzle1result = sum(c==1)*sum(c==2);
