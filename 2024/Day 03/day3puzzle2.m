4e7e76e39be74e220b8b4cf7dd705a2ab017e2c4
Daniel Breslan <daniel@breslan.co.uk>
2024-12-08 20:37:37 +0000

    24.03

--- 2024/Day 03/day3puzzle2.m
+++ 2024/Day 03/day3puzzle2.m
@@ -1,5 +1,13 @@
 %% day3puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").join("");
+day3puzzle1result = multiplications(data)
+day3puzzle2result = multiplications(regexprep(data,"don't\(\).*?do\(\)",""))
 
-
-day3puzzle2result = 0;
+function out = multiplications(in)
+[s,e] = regexp(in,"mul\(\d+,\d+\)");
+out = 0;
+for idx = 1:numel(s)
+    out = out + ...
+        prod(double(in.extractBetween(s(idx),e(idx)).extract(digitsPattern)));
+end
+end
