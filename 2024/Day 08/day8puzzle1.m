70023061c769a8161a2e201ed63b7db9216ddddc
Daniel Breslan <daniel@breslan.co.uk>
2024-12-08 15:09:53 +0000

    24.08

--- 2024/Day 08/day8puzzle1.m
+++ 2024/Day 08/day8puzzle1.m
@@ -1,5 +1,32 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
-day8puzzle1result = 0;
+map = char(readlines("input.txt"));
+antennaTypes = unique(map);
+antennaTypes(antennaTypes=='.') = [];
+antiNodeLocs = [];
+for idx = 1:numel(antennaTypes)
+    thisAntennaType = antennaTypes(idx);
+    [r,c] = find(map == thisAntennaType);
+    theseAntiNodesLocs = [];
+    li = 1;
+    for ai = 1:numel(r)
+        for aii = (ai+1):numel(r)
+          dr = r(ai) - r(aii);
+          dc = c(ai) - c(aii);
+          cd = gcd(dr,dc);
+          dr = dr / cd;
+          dc = dc / cd;
+          theseAntiNodesLocs(li,:) = [r(aii) - dr,...
+              c(aii) - dc]; %#ok<*SAGROW>
+          theseAntiNodesLocs(li + 1,:) = [r(ai) + dr,...
+              c(ai) + dc];
+          li = li + 2;
+        end
+    end
+    antiNodeLocs = unique([antiNodeLocs;theseAntiNodesLocs(...
+        theseAntiNodesLocs(:,1) > 0 &...
+        theseAntiNodesLocs(:,1) <= size(map,1) &...
+        theseAntiNodesLocs(:,2) > 0 &...
+        theseAntiNodesLocs(:,2) <= size(map,2),:)],...
+        "rows");
+end
+day8puzzle1result = height(antiNodeLocs)
--- 2024/Day 08/day8puzzle2.m
+++ 2024/Day 08/day8puzzle2.m
@@ -1,5 +1,32 @@
 %% day8puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
-day8puzzle2result = 0;
+map = char(readlines("input.txt"));
+antennaTypes = unique(map);
+antennaTypes(antennaTypes=='.') = [];
+antiNodeLocs = [];
+for idx = 1:numel(antennaTypes)
+    thisAntennaType = antennaTypes(idx);
+    [r,c] = find(map == thisAntennaType);
+    theseAntiNodesLocs = [];
+    li = 1;
+    for ai = 1:numel(r)
+        for aii = (ai+1):numel(r)
+          dr = r(ai) - r(aii);
+          dc = c(ai) - c(aii);
+          cd = gcd(dr,dc);
+          dr = dr / cd;
+          dc = dc / cd;
+          theseAntiNodesLocs(li + (0:49),:) = [r(aii) - dr * (0:49)',...
+              c(aii) - dc * (0:49)']; %#ok<*SAGROW>
+          theseAntiNodesLocs(li + (50:99),:) = [r(ai) + dr * (0:49)',...
+              c(ai) + dc * (0:49)'];
+          li = li + 100;
+        end
+    end
+    antiNodeLocs = unique([antiNodeLocs;theseAntiNodesLocs(...
+        theseAntiNodesLocs(:,1) > 0 &...
+        theseAntiNodesLocs(:,1) <= size(map,1) &...
+        theseAntiNodesLocs(:,2) > 0 &...
+        theseAntiNodesLocs(:,2) <= size(map,2),:)],...
+        "rows");
+end
+day8puzzle2result = height(antiNodeLocs)
