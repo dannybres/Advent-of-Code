bb8e9604e7fd43b40c5cf6d1703ff4ea444058e8
Daniel Breslan <daniel@breslan.co.uk>
2024-12-13 09:22:49 +0000

    24.13

--- 2024/Day 13/day13puzzle1.m
+++ 2024/Day 13/day13puzzle1.m
@@ -1,5 +1,11 @@
 %% day13puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
+data = readlines("input.txt");
 day13puzzle1result = 0;
+for idx = 1:4:numel(data)
+    nums = double([data(idx).extractBetween("X",",") data(idx).extractAfter("Y") data(idx+1).extractBetween("X",",") data(idx+1).extractAfter("Y") data(idx+2).extractBetween("X=",",") data(idx+2).extractAfter("Y=")]);
+    presses = nums([1 3;2 4]) \ nums([5 6])';
+    if all(abs(presses - round(presses)) < 1e-3)
+        day13puzzle1result = day13puzzle1result + sum(presses .* [3;1]);
+    end
+end
+day13puzzle1result 
--- 2024/Day 13/day13puzzle2.m
+++ 2024/Day 13/day13puzzle2.m
@@ -1,5 +1,12 @@
 %% day13puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
+ddata = readlines("input.txt");
 day13puzzle2result = 0;
+for idx = 1:4:numel(data)
+    nums = double([data(idx).extractBetween("X",",") data(idx).extractAfter("Y") data(idx+1).extractBetween("X",",") data(idx+1).extractAfter("Y") data(idx+2).extractBetween("X=",",") data(idx+2).extractAfter("Y=")]);
+    nums(5:6) = nums(5:6) + 10000000000000;
+    presses = nums([1 3;2 4]) \ nums([5 6])';
+    if all(abs(presses - round(presses)) < 1e-3)
+        day13puzzle2result = day13puzzle2result + sum(presses .* [3;1]);
+    end
+end
+day13puzzle2result 
