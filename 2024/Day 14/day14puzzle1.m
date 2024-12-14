19ba8358236cb30cb8a459da8de71a660d550201
Daniel Breslan <daniel@breslan.co.uk>
2024-12-14 20:32:06 +0000

    24.14

--- 2024/Day 14/day14puzzle1.m
+++ 2024/Day 14/day14puzzle1.m
@@ -1,5 +1,16 @@
 %% day14puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
-day14puzzle1result = 0;
+robotData = readlines("input.txt").erase("p=").replace(" v=",",").split(",").double();
+width = 101;
+height = 103;
+per = reshape([true true; ...
+    true false; ...
+    false true; ...
+    false false]',1 ...
+    ,2,[]);
+seconds = 100;
+location = robotData(:,1:2) + seconds * robotData(:,3:4);
+location = mod(location,[width,height]);
+location = location(~any(location == [floor(width/2) floor(height/2)],2),:);
+location = location < [floor(width/2) floor(height/2)];
+num = squeeze(sum(all(location == per,2)));
+prod(num) 
--- 2024/Day 14/day14puzzle2.m
+++ 2024/Day 14/day14puzzle2.m
@@ -1,5 +1,40 @@
 %% day14puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").erase("p=").replace(" v=",",").split(",").double();
+w = 101;
+h = 103;
+%% min safety factor - with a tree formed, some pixels will be grouped together, product of values (that sum to same value) is lowest when unevenly spaced (random spacing will give largest safety factors as (0.25 x n)^4).
+per = reshape([true true; ...
+    true false; ...
+    false true; ...
+    false false]',1 ...
+    ,2,[]);
+minsf = inf;
+minstep = nan;
+for n = 1:(w*h)
+    l = data(:,1:2) + n * data(:,3:4);
+    l = mod(l,[w h]);
+    l = l(~any(l == [floor(w/2) floor(h/2)],2),:);
+    l = l < [floor(w/2) floor(h/2)];
+    num = squeeze(sum(all(l == per,2)));
+    sf = prod(num);
+    if sf < minsf
+        minsf = sf;
+        minstep = n;
+    end
+end
+minstep
 
+%% all robots in unique locations, possible solution that work - AoC things
+for n = 1:(w*h)
+    l = data(:,1:2) + n * data(:,3:4);
+    l = mod(l,[w h]);
+    if height(unique(l,"rows")) == height(data)
+        allSeperate = n
+        break
+    end
+end
 
-day14puzzle2result = 0;
+%% minVariance in x and y, tree will group some values, so variance will drop
+x = mod(data(:,1) + (1:w*h) .* data(:,3),w);
+y = mod(data(:,2) + (1:w*h) .* data(:,4),h);
+minVstep = find(min(var(x)) == var(x) & min(var(y)) == var(y))
