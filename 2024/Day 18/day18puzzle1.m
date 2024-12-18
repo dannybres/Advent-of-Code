a5a3677dfae1f8c12448f3a599ab1f80380f19f1
Daniel Breslan <daniel@breslan.co.uk>
2024-12-18 10:33:49 +0000

    24.18

--- 2024/Day 18/day18puzzle1.m
+++ 2024/Day 18/day18puzzle1.m
@@ -1,5 +1,30 @@
 %% day18puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(",").double();
+dim = 70;
+time = 1024;
+q = nan(1e4,3);
+q(1,:) = [0 0 0];
+qidx = 1;
+qiidx = 1;
 
+clear result
+while 1
+if exist('result','var'),break,end
+item = q(qidx,:);
+if isnan(item(1)), break,end
+cl = item(2:3);
+cc = item(1);
+for d = [-1 0; 1 0; 0 -1; 0 1]'
+    dir = d';
+    nl = cl + dir;
+    if any(nl < 0) || any(nl > dim), continue, end
+    if any(all(nl == data(1:time,:),2)), continue, end
+    if any(all(nl == q(1:qiidx,2:3),2)), continue, end
+    if all(nl == [dim dim]),result = cc+1; end
+    qiidx = qiidx + 1;
+    q(qiidx,:) = [cc + 1, nl];
+end
+qidx = qidx + 1;
+end
 
-day18puzzle1result = 0;
+day18puzzle1result = result
--- 2024/Day 18/day18puzzle2.m
+++ 2024/Day 18/day18puzzle2.m
@@ -1,5 +1,26 @@
 %% day18puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
-day18puzzle2result = 0;
+profile on
+data = readlines("input.txt").split(",").double();
+dim = 71;
+n = height(data);
+map = true(dim,dim,n);
+for idx = 1:height(data)
+    map(sub2ind(size(map),1+data(1:idx,1),1+data(1:idx,2),idx)) = false;
+end
+reachable = false(size(map,1),size(map,2),n);
+reachable(1,1,:) = 1;
+m = [0 1 0; 1 1 1; 0 1 0];
+for idx = size(reachable,3):-1:1
+    while true
+        seen = nnz(reachable(:,:,idx));
+        reachable(:,:,idx) = conv2(reachable(:,:,idx),m,'same') & map(:,:,idx);
+        if seen == nnz(reachable(:,:,idx))
+            break
+        end
+    end
+    if reachable(end,end,idx)
+        break
+    end
+end
+day18puzzle2result = string(data(idx+1,:)).join(",")
+profile viewer
