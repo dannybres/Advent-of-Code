50f9a00ec78896852a6ce49937dce364c79cf944
Daniel Breslan <daniel@breslan.co.uk>
2024-12-19 09:08:46 +0000

    24.13 tidied up

--- 2024/Day 13/day13puzzle2.m
+++ 2024/Day 13/day13puzzle2.m
@@ -1,5 +1,5 @@
 %% day13puzzle2 - Daniel Breslan - Advent Of Code 2024
-ddata = readlines("input.txt");
+data = readlines("input.txt");
 day13puzzle2result = 0;
 for idx = 1:4:numel(data)
     nums = double([data(idx).extractBetween("X",",") data(idx).extractAfter("Y") data(idx+1).extractBetween("X",",") data(idx+1).extractAfter("Y") data(idx+2).extractBetween("X=",",") data(idx+2).extractAfter("Y=")]);
