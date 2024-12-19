eb1dccf7a528c4d246718ba0d20928c121db70bc
Daniel Breslan <daniel@breslan.co.uk>
2024-12-19 11:24:26 +0000

    Fixed a bug where I didnt add to queue, if a cell and direction was already queued. This does not work for Dijkstra as I missed the oppurtunity to queue a cell with a lower cost than the queued one. What you need to do is not queue add to the queue once the cell has been processed (as that is the guaranteed lowst cost to that state).

--- 2024/Day 16/day16puzzle1.m
+++ 2024/Day 16/day16puzzle1.m
@@ -3,11 +3,15 @@ m = char(readlines("input.txt"));
 rd = [0 1];
 [cr,cc] = find(m == 'S');
 [tr,tc] = find(m == 'E');
-n = 10e3;
-q = nan(n,6);
-q(1,:) = [0 cr cc rd nan];
+n = 10e6;
+q = nan(n,5);
+q(1,:) = [0 cr cc rd ];
 qidx = 1;
 reachTarget = false;
+
+seen = false(size(m,1),size(m,2),4); % state whether proccessed (once it is
+% processed, we know this is the shortest path to that square with that dir
+% so no need to queue again as it can only be longer.
 while true
     toProcessIdx = find(q(1:qidx,1) == min(q(1:qidx,1)));
     for idx = 1:height(toProcessIdx)
@@ -16,24 +20,29 @@ while true
         cr = item(2);
         cc = item(3);
         cdir = item(4:5);
-        for d = [-1 0;1 0;0 -1;0 1]'
-            nr = cr + d(1);
-            nc = cc + d(2);
-            if m(nr,nc) == '#', continue, end
-            if all(cdir == -d'), continue, end
-            if all([nr nc] == [tr tc])
+        seen(cr,cc,(sum(cdir .* [3 1]) + 5) / 2) = true; % this maths maps
+        % (-1 0), (0 -1), (0 1), (1 0) to 1 to 4, to save a lookup, used
+        % later to when checking if seen
+        for move = [cr cc -cdir(2) cdir(1) ccost + 1000;...
+                cr cc cdir(2) -cdir(1) ccost + 1000;...
+                [cr cc] + cdir, cdir, ccost + 1]'% dont move and turn, 
+            % dont move and turn, move forwards
+            % (c -r) is a clockwise turn and (-c r) is a c.clockwise one
+            nextRow = move(1);
+            nextCol = move(2);
+            nextDir = move(3:4)';
+            ncost = move(5);
+            if m(nextRow,nextCol) == '#', continue, end % if wall skip
+            if all([nextRow nextCol] == [tr tc]) % reached the end
                 reachTarget = true;
                 day16puzzle1result = ccost + 1;
                 break
             end
-            if any(all([nr,nc,d']==q(1:qidx-1,2:5),2)),break,end
+            if seen(nextRow,nextCol,(sum(nextDir .* [3 1]) + 5) / 2)
+                continue
+            end % processed already, do not queue again, can only be longer
             qidx = qidx + 1;
-            if all(d' == cdir)
-                q(qidx,:) = [ccost+1,nr,nc,d',toProcessIdx(idx)];
-            else
-
-                q(qidx,:) = [ccost+1001,nr,nc,d',toProcessIdx(idx)];
-            end
+            q(qidx,:) = [ncost,nextRow,nextCol,nextDir]; % add to queue
         end
     end
     if reachTarget, break, end
@@ -41,3 +50,10 @@ while true
 end
 
 day16puzzle1result
+
+% mm = ones(size(m));
+% mm(sub2ind(size(mm),q(1:qidx,2),q(1:qidx,3))) = 0;
+% mm(m=='#') = 0.5;
+% imagesc(mm)
+% drawnow
+% shg
