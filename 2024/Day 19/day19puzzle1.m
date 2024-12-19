4ff1c863746351077eb5febb21d3cfa832352d78
Daniel Breslan <daniel@breslan.co.uk>
2024-12-19 08:44:32 +0000

    24.19

--- 2024/Day 19/day19puzzle1.m
+++ 2024/Day 19/day19puzzle1.m
@@ -1,5 +1,37 @@
 %% day19puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
+data = readlines("input.txt");
+t = data(1).split(", ");
+d = data(3:end);
 day19puzzle1result = 0;
+day19puzzle1result = 0;
+for idx = 1:numel(d)
+    day19puzzle1result = day19puzzle1result + isPossible(d(idx),t);
+end
+day19puzzle1result
+
+function possible = isPossible(design,towels)
+persistent cache
+if isempty(cache)
+    cache = configureDictionary("string","logical");
+elseif isKey(cache,design)
+        possible = cache(design);
+        return
+end
+possible = false;
+for t = towels'
+    if design.endsWith(t)
+        trimmedDesign = design.extractBefore(...
+            strlength(design)-strlength(t)+1);
+        if trimmedDesign == ""
+            possible = true;
+            break
+        else
+            possible = isPossible(trimmedDesign,towels);
+        end
+        if possible
+            break
+        end
+    end
+end
+cache(design) = possible;
+end
--- 2024/Day 19/day19puzzle2.m
+++ 2024/Day 19/day19puzzle2.m
@@ -1,5 +1,34 @@
 %% day19puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
+data = readlines("input.txt");
+t = data(1).split(", ");
+d = data(3:end);
+day19puzzle1result = 0;
 day19puzzle2result = 0;
+for idx = 1:numel(d)
+    idx
+    day19puzzle2result = day19puzzle2result + towelCombinations(d(idx),t);
+end
+day19puzzle2result
+
+function number = towelCombinations(design,towels)
+persistent cache
+if isempty(cache)
+    cache = configureDictionary("string","double");
+elseif isKey(cache,design)
+        number = cache(design);
+        return
+end
+number = 0;
+for t = towels'
+    if design.endsWith(t)
+        trimmedDesign = design.extractBefore(...
+            strlength(design)-strlength(t)+1);
+        if trimmedDesign == ""
+            number = number + 1;
+        else
+            number = number + towelCombinations(trimmedDesign,towels);
+        end
+    end
+end
+cache(design) = number;
+end
