34adfc4f0d75b086d3da78f0bc66eb11be62fdd2
Daniel Breslan <daniel@breslan.co.uk>
2024-12-20 15:27:25 +0000

    24.20

--- 2024/Day 20/day20puzzle2.m
+++ 2024/Day 20/day20puzzle2.m
@@ -1,5 +1,39 @@
 %% day20puzzle2 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
+m = char(readlines("input.txt"));
+[r,c] = find(m == 'S');
+[tr,tc] = find(m == 'E');
+m(m=='S') = '.';
+m(m=='E') = '.';
 
+cheatAllowance = 20;
+
+mm = nan(size(m));
+mm(m=='#') = inf;
+
+path = nan(round(numel(m)/4),2);
+pidx = 0;
+for idx = 0:1e6
+    mm(r,c) = idx;
+    pidx = pidx + 1;
+    path(pidx,:) = [r c];
+    if all([r c] == [tr tc]), break, end
+    for dir = [-1 0;1 0;0 -1;0 1]'
+        nr = r + dir(1); nc = c + dir(2);
+        if isnan(mm(nr,nc))
+            r = nr; c = nc;
+            break
+        end
+    end
+end
 
 day20puzzle2result = 0;
+path = path(1:pidx,:);
+nc = (0:height(path)-1)';
+for idx = 1:height(path)
+    manhattenDistanceToAll = ...
+        abs(path(:,1) - path(idx,1)) + abs(path(:,2) - path(idx,2));
+    costSaving = nc(manhattenDistanceToAll <= cheatAllowance) - ...
+        (nc(idx) + manhattenDistanceToAll(manhattenDistanceToAll <= cheatAllowance));
+    day20puzzle2result = day20puzzle2result + nnz(costSaving >= 100);
+end
+day20puzzle2result
