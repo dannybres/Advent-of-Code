71822456165bfd7ccbf64deb9db3033227d543ae
Daniel Breslan <daniel@breslan.co.uk>
2024-12-23 15:27:29 +0000

    fixed a bug?

--- 2024/Day 23/day23puzzle2.m
+++ 2024/Day 23/day23puzzle2.m
@@ -10,19 +10,20 @@ end
 
 
 for nidx = 1:height(data)
-    newNeighbour = check(data(nidx,:),neighbors);
+    newNeighbour = check(data(nidx,:),data,neighbors);
     if numel(newNeighbour) + 2 > numel(party)
         party = [data(nidx,:) newNeighbour];
     end
 end
 day23puzzle2result = sort(party).join(",")
 
-function newNodes = check(nodes,neighbors)
-newNodes = [neighbors(nodes(1)).split(",")];
-newNodes = newNodes(ismember(newNodes,neighbors(nodes(1)).split(",")));
+function newNodes = check(nodes,data,neighbors)
+n1 = neighbors(nodes(1)).split(",");
+n2 = neighbors(nodes(2)).split(",");
+newNodes = n1(ismember(n1,n2));
 newNodes(ismember(newNodes,nodes)) = [];
-newNodes = reshape(unique(newNodes),1,[]);
 
+newNodes = reshape(newNodes,1,[]);
 if isempty(newNodes), return, end
 conn = false(numel(newNodes),numel(newNodes));
 for ridx = 1:numel(newNodes)
