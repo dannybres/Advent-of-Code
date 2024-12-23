937fa87de241ff5aaa26ae99b407671198f537f8
Daniel Breslan <daniel@breslan.co.uk>
2024-12-23 13:37:13 +0000

    24.23

--- 2024/Day 23/day23puzzle1.m
+++ 2024/Day 23/day23puzzle1.m
@@ -1,5 +1,21 @@
 %% day23puzzle1 - Daniel Breslan - Advent Of Code 2024
-data = readlines("inputDemo.txt")
-
-
-day23puzzle1result = 0;
+data = readlines("input.txt").split("-");
+nodes = unique(data(:));
+parties = repmat("",1e5,3);
+iidx = 1;
+for n = nodes'
+    o = data(any(n == data,2),:);
+    o = o(:);
+    o(o == n) = [];
+    o = o + "-" + o';
+    o = o (:);
+    o = o(ismember(o(:),data.join("-")),:);
+    if ~isempty(o)
+        h = height(o);
+        parties(iidx:iidx + h - 1,:) = ...
+            [repmat(n,h,1), reshape(o.split("-"),[],2)];
+        iidx = iidx + h;
+    end
+end
+day23puzzle1result = nnz(any(unique(sort(...
+    parties(1:iidx-1,:),2),'row').startsWith("t"),2))
--- 2024/Day 23/day23puzzle2.m
+++ 2024/Day 23/day23puzzle2.m
@@ -1,22 +1,26 @@
 %% day23puzzle2 - Daniel Breslan - Advent Of Code 2024
-profile on
 data = readlines("input.txt").split("-");
 nodes = unique(data(:));
 party = [];
+
+neighbors = configureDictionary("string","string");
+for idx = 1:numel(nodes)
+    neighbors(nodes(idx)) = neighboursOf(nodes(idx),data).join(",");
+end
+
+
 for nidx = 1:height(data)
-    newNeighbour = check(data(nidx,:),data);
+    newNeighbour = check(data(nidx,:),data,neighbors);
     if numel(newNeighbour) + 2 > numel(party)
         party = [data(nidx,:) newNeighbour];
     end
 end
 day23puzzle2result = sort(party).join(",")
-profile viewer
 
 
-
-function newNodes = check(nodes,data)
+function newNodes = check(nodes,data,neighbors)
 for idx = 1:numel(nodes)
-    nn = neighboursOf(nodes(idx),data);
+    nn = neighbors(nodes(idx)).split(",");
     nn(ismember(nn,nodes)) = [];
     if exist("newNodes","var")
         newNodes = nn(ismember(nn,newNodes));
@@ -29,7 +33,7 @@ if isempty(newNodes), return, end
 conn = false(numel(newNodes),numel(newNodes));
 for ridx = 1:numel(newNodes)
     node = newNodes(ridx);
-    conn(ridx,:) = any(neighboursOf(node,data) == newNodes);
+    conn(ridx,:) = any(neighbors(node).split(",") == newNodes);
     conn(ridx,ridx) = true;
 end
 if max(sum(conn)) == 1
@@ -37,13 +41,9 @@ if max(sum(conn)) == 1
 else
     newNodes = newNodes(sum(conn) == max(sum(conn)));
 end
-
 end
 
 
-g = graph;
-g = g.addedge(data(:,1),data(:,2));
-plot(g)
 function neighbours = neighboursOf(node,data)
 neighbours = data(any(node == data,2),:);
 neighbours = neighbours(:);
