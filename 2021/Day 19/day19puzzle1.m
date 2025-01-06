48469986541fcbc2b2d3199076dc0c2d742da8fd
Daniel Breslan <daniel@breslan.co.uk>
2025-01-06 09:41:20 +0000

    21.19 tidied up

--- 2021/Day 19/day19puzzle1.m
+++ 2021/Day 19/day19puzzle1.m
@@ -1,15 +1,15 @@
-tic
+%% day19 - Daniel Breslan - Advent Of Code 2021
 data = readlines("input.txt");
 data(data == "") = [];
 titleIdx = find(data.contains("--"));
 reports = cell(1,numel(titleIdx));
 for idx = 1:numel(titleIdx)
     try
-        e = titleIdx(idx + 1) - 1;
+        endIdx = titleIdx(idx + 1) - 1;
     catch
-        e = numel(data);
+        endIdx = numel(data);
     end
-    reports{idx} = data(titleIdx(idx)+1:e).split(",").double();
+    reports{idx} = data(titleIdx(idx)+1:endIdx).split(",").double();
 end
 
 knowBeacons = reports{1};
@@ -20,17 +20,21 @@ while any(isnan(scannerLocations(:,1)))
     for baseIdx = find(~isnan(scannerLocations(:,1)))'
         for baseRelativeIdx = 1:height(reports{baseIdx})
             baseReports = reports{baseIdx};
-            baseReports = abs(baseReports - baseReports(baseRelativeIdx,:));
+            baseReports = abs(baseReports - ...
+                baseReports(baseRelativeIdx,:));
             for otherIdx = find(isnan(scannerLocations(:,1)))'
                 found = false;
                 if ~couldOverlap(reports,[baseIdx,otherIdx])
                     continue
                 end
-                for otherRelativeIdx = 1:height(reports{otherIdx}) % other relative
+                for otherRelativeIdx = 1:height(reports{otherIdx}) 
+                    % other relative
                     otherReports = reports{otherIdx};
-                    otherReports = abs(otherReports - otherReports(otherRelativeIdx,:));
+                    otherReports = abs(otherReports - ...
+                        otherReports(otherRelativeIdx,:));
                     for pidx = perms(1:3)' % column order
-                        if nnz(ismember(otherReports(:,pidx),baseReports,"rows")) == 12
+                        if nnz(ismember(otherReports(:,pidx), ...
+                                baseReports,"rows")) == 12
                             found = true;
                             break
                         end
@@ -60,14 +64,16 @@ while any(isnan(scannerLocations(:,1)))
         newScanner = knownBeacons + otherMatched;
 
         axisInversionNeeded = ~all(newScanner(1,:) == newScanner);
-        otherMatched(:,axisInversionNeeded) = -otherMatched(:,axisInversionNeeded);
+        otherMatched(:,axisInversionNeeded) = ...
+            -otherMatched(:,axisInversionNeeded);
         newScanner = knownBeacons + otherMatched;
         assert(all(newScanner(1,:) == newScanner,'all'));
 
-        scannerLocations(otherIdx,:) = newScanner(1,:);%, pidx', double(~axisInversionNeeded)]
+        scannerLocations(otherIdx,:) = newScanner(1,:);
 
         reports{otherIdx} = reports{otherIdx}(:,pidx);
-        reports{otherIdx}(:,~axisInversionNeeded) = -reports{otherIdx}(:,~axisInversionNeeded);
+        reports{otherIdx}(:,~axisInversionNeeded) = ...
+            -reports{otherIdx}(:,~axisInversionNeeded);
         if ~any(isnan(scannerLocations(:,1)))
             break
         end
@@ -83,17 +89,17 @@ for idx = 1:height(scannerLocations)
 end
 
 beacons = unique(allbeacons,"rows");
-height(beacons)
+day19part1result = height(beacons)
 
-d = 0;
+day19part2result = 0;
 for f = 1:height(scannerLocations)
     for t = 1:height(scannerLocations)
         if f <= t, continue, end
-        d = max(d,sum(abs(scannerLocations(f,:) - scannerLocations(t,:))));
+        day19part2result = max(day19part2result,...
+            sum(abs(scannerLocations(f,:) - scannerLocations(t,:))));
     end
 end
-d
-toc
+day19part2result
 
 function could = couldOverlap(reports,idx)
 idx = sort(idx);
@@ -106,16 +112,17 @@ if isKey(cache,key)
     could = cache(key);
     return
 end
-a = reports{idx(1)};
-hashA = sort(abs([reshape(a(:,1) - a(:,1)',[],1)...
-    reshape(a(:,2) - a(:,2)',[],1)...
-    reshape(a(:,3) - a(:,3)',[],1)]),2);
+hashes = reports{idx(1)};
+hashA = sort(abs([reshape(hashes(:,1) - hashes(:,1)',[],1)...
+    reshape(hashes(:,2) - hashes(:,2)',[],1)...
+    reshape(hashes(:,3) - hashes(:,3)',[],1)]),2);
 
-a = reports{idx(2)};
-hashB = sort(abs([reshape(a(:,1) - a(:,1)',[],1)...
-    reshape(a(:,2) - a(:,2)',[],1)...
-    reshape(a(:,3) - a(:,3)',[],1)]),2);
+hashes = reports{idx(2)};
+hashB = sort(abs([reshape(hashes(:,1) - hashes(:,1)',[],1)...
+    reshape(hashes(:,2) - hashes(:,2)',[],1)...
+    reshape(hashes(:,3) - hashes(:,3)',[],1)]),2);
 
-could = nnz(any(string(hashB).join(".") == string(hashA).join(".")')) >= nchoosek(12,2);
+could = nnz(any(string(hashB).join(".") == string(hashA).join(".")')) ...
+    >= nchoosek(12,2);
 cache(key) = could;
 end
