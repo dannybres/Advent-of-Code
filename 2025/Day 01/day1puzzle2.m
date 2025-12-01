767c640aa838802769e6a9bb24ff8b3058950a70
Daniel Breslan <daniel@breslan.co.uk>
2025-12-01 13:13:50 +0000

    Vectorised Day 2

--- 2025/Day 01/day1puzzle2.m
+++ 2025/Day 01/day1puzzle2.m
@@ -1,28 +1,15 @@
 %% day1puzzle2 - Daniel Breslan - Advent Of Code 2025
-clc
 data = readlines("input.txt");
 dir = data.extract(1);
 num = str2double(data.extractAfter(1));
+num(dir  == "L") =  -num(dir  == "L");
 
-dialLocation = 50;
-counterOfZeroPasses = 0;
+startingVal = 50;
 
-for idx = 1:numel(dir)
-    st = dialLocation;
-    if dir(idx) == "L"
-        dialLocation = dialLocation - num(idx);
-    else
-        dialLocation = dialLocation + num(idx);
-    end
-    en = dialLocation;
-    counterOfZeroPasses = counterOfZeroPasses + floor(abs(en)/100);
-    if en < 1
-        counterOfZeroPasses = counterOfZeroPasses + 1;
-    end
-    if st == 0 && en < 0
-        counterOfZeroPasses = counterOfZeroPasses - 1;
-    end
-    dialLocation = mod(dialLocation,100);
-end
+startVal = [startingVal;mod(startingVal + cumsum(num(1:end-1)),100)];
 
-day1puzzle2result = counterOfZeroPasses
+clicksToNext = startVal;
+clicksToNext(num > 0) = 100 - clicksToNext(num > 0);
+clicksToNext(clicksToNext == 0) = 100;
+
+day1puzzle2result = sum(floor((abs(num) - clicksToNext) ./ 100) + 1)
