1cf174048ac5ae8fa3164295851e3450eb4c2308
Daniel Breslan <daniel@breslan.co.uk>
2025-12-02 08:50:53 +0000

    2025 Day 2 complete

--- 2025/Day 02/day2puzzle1.m
+++ 2025/Day 02/day2puzzle1.m
@@ -1,5 +1,18 @@
 %% day2puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
+data = readlines("input.txt").split(",").split("-").double();
 
 day2puzzle1result = 0;
+for dIdx = 1:height(data)
+    ids = string(data(dIdx,1):data(dIdx,2));
+    idLengths = strlength(ids);
+
+    ids(logical(mod(idLengths,2))) = []; % remove odd lengths
+    idLengths(logical(mod(idLengths,2))) = [];
+
+    halfIdLength = idLengths/2;
+    day2puzzle1result = day2puzzle1result + ...
+        sum(ids(ids.extractBefore(halfIdLength+1) == ...
+        ids.extractAfter(halfIdLength)).double);
+end
+format longg
+day2puzzle1result
--- 2025/Day 02/day2puzzle2.m
+++ 2025/Day 02/day2puzzle2.m
@@ -1,5 +1,22 @@
 %% day2puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
+data = readlines("input.txt").split(",").split("-").double();
 
 day2puzzle2result = 0;
+for dIdx = 1:height(data)
+    ids = string(data(dIdx,1):data(dIdx,2));
+    idLengths = strlength(ids);
+    idUniqueLengths = unique(idLengths);
+
+    invalidIds = [];
+    for idl = idUniqueLengths
+        sids = char(ids(idl == idLengths)');
+        options = find(~mod(idl./(1:floor(idl/2)),1));
+        for op = options
+            b = reshape(sids,height(sids),op,[]);
+            invalidIds = [invalidIds;string(sids(all(all(b == b(:,:,1),2),3),:)).double()];
+        end
+    end
+    day2puzzle2result = day2puzzle2result + sum(unique(invalidIds));
+end
+format longg
+day2puzzle2result
