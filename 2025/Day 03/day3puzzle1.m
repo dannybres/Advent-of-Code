5accad295703e8930ebdee853e88fbe29d45a888
Daniel Breslan <daniel@breslan.co.uk>
2025-12-03 07:43:06 +0000

    Day 3 complete

--- 2025/Day 03/day3puzzle1.m
+++ 2025/Day 03/day3puzzle1.m
@@ -1,5 +1,14 @@
 %% day3puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").char() - '0';
+n = 2;
+r = nan(height(data),n);
+for idx = 1:height(data)
+    s = 1;
+    for rIdx = 1:n
+        r(idx,rIdx) = max(data(idx,s:end-n+rIdx));
+        s = find(data(idx,s:end) == r(idx,rIdx),1) + s;
+    end
+end
+sum(r .* 10.^(n-1:-1:0),'all')
 
 
-day3puzzle1result = 0;
--- 2025/Day 03/day3puzzle2.m
+++ 2025/Day 03/day3puzzle2.m
@@ -1,5 +1,12 @@
 %% day3puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
-
-day3puzzle2result = 0;
+data = readlines("input.txt").char() - '0';
+n = 12;
+r = nan(height(data),n);
+for idx = 1:height(data)
+    s = 1;
+    for rIdx = 1:n
+        r(idx,rIdx) = max(data(idx,s:end-n+rIdx));
+        s = find(data(idx,s:end) == r(idx,rIdx),1) + s;
+    end
+end
+sum(r .* 10.^(n-1:-1:0),'all')
