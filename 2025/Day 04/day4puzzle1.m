14dc14208608b25381df65ba865f9f208ff91d38
Daniel Breslan <daniel@breslan.co.uk>
2025-12-04 06:53:08 +0000

    Day 4 done

--- 2025/Day 04/day4puzzle1.m
+++ 2025/Day 04/day4puzzle1.m
@@ -1,5 +1,6 @@
 %% day4puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
-
-day4puzzle1result = 0;
+map = char(readlines("input.txt")) == '@';
+cn = ones(3); cn(2,2) = 0;
+numAdj = conv2(map,cn,"same");
+numAdj(~map) = inf;
+day4puzzle1result = nnz(numAdj .* map < 4)
--- 2025/Day 04/day4puzzle2.m
+++ 2025/Day 04/day4puzzle2.m
@@ -1,5 +1,14 @@
 %% day4puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+map = char(readlines("input.txt")) == '@';
+cn = ones(3); cn(2,2) = 0;
 
-
-day4puzzle2result = 0;
+day4puzzle1result = 0;
+canRemove = 1;
+while nnz(canRemove)
+    numAdj = conv2(map,cn,"same");
+    numAdj(~map) = inf;
+    canRemove = numAdj .* map < 4;
+    day4puzzle1result = day4puzzle1result + nnz(canRemove);
+    map(canRemove) = 0;
+end
+day4puzzle1result
