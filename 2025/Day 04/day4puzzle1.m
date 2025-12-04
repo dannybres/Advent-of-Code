045cefe21012597d6955b6b45b3968f05d25461c
Daniel Breslan <daniel@breslan.co.uk>
2025-12-04 08:43:25 +0000

    simplified it all, while true with a break and kept initial count, not constant sumation. removed the inf for blanks and &'ed with the map

--- 2025/Day 04/day4puzzle1.m
+++ 2025/Day 04/day4puzzle1.m
@@ -1,5 +1,3 @@
 %% day4puzzle1 - Daniel Breslan - Advent Of Code 2025
 map = char(readlines("input.txt")) == '@';
-numAdj = conv2(map,ones(3),"same");
-numAdj(~map) = inf;
-day4puzzle1result = nnz(numAdj .* map < 5)
+day4puzzle1result = nnz(map & (conv2(map,ones(3),"same") .* map < 5))
--- 2025/Day 04/day4puzzle2.m
+++ 2025/Day 04/day4puzzle2.m
@@ -1,13 +1,10 @@
 %% day4puzzle2 - Daniel Breslan - Advent Of Code 2025
 map = char(readlines("input.txt")) == '@';
 
-day4puzzle1result = 0;
-canRemove = 1;
-while nnz(canRemove)
-    numAdj = conv2(map,ones(3),"same");
-    numAdj(~map) = inf;
-    canRemove = numAdj .* map < 5;
-    day4puzzle1result = day4puzzle1result + nnz(canRemove);
-    map(canRemove) = 0;
+initialCount = nnz(map);
+while true
+    canRemove = map & (conv2(map,ones(3),"same") .* map < 5);
+    map(canRemove) = false;
+    if ~nnz(canRemove), break, end
 end
-day4puzzle1result
+day4puzzle1result = initialCount - nnz(map)
