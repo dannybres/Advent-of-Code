d5a1595cc86e80459f8249033333673badd23416
Daniel Breslan <daniel@breslan.co.uk>
2025-12-04 07:45:27 +0000

    used a conv of ones and did <4 as we are not looking for neighbours to empty, but adjacent to filled.

--- 2025/Day 04/day4puzzle1.m
+++ 2025/Day 04/day4puzzle1.m
@@ -1,6 +1,5 @@
 %% day4puzzle1 - Daniel Breslan - Advent Of Code 2025
 map = char(readlines("input.txt")) == '@';
-cn = ones(3); cn(2,2) = 0;
-numAdj = conv2(map,cn,"same");
+numAdj = conv2(map,ones(3),"same");
 numAdj(~map) = inf;
-day4puzzle1result = nnz(numAdj .* map < 4)
+day4puzzle1result = nnz(numAdj .* map < 5)
--- 2025/Day 04/day4puzzle2.m
+++ 2025/Day 04/day4puzzle2.m
@@ -1,13 +1,12 @@
 %% day4puzzle2 - Daniel Breslan - Advent Of Code 2025
 map = char(readlines("input.txt")) == '@';
-cn = ones(3); cn(2,2) = 0;
 
 day4puzzle1result = 0;
 canRemove = 1;
 while nnz(canRemove)
-    numAdj = conv2(map,cn,"same");
+    numAdj = conv2(map,ones(3),"same");
     numAdj(~map) = inf;
-    canRemove = numAdj .* map < 4;
+    canRemove = numAdj .* map < 5;
     day4puzzle1result = day4puzzle1result + nnz(canRemove);
     map(canRemove) = 0;
 end
