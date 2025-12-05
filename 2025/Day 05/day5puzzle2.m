096fce55455fff1505a8ab0084797956869e3f19
Daniel Breslan <daniel@breslan.co.uk>
2025-12-05 16:34:53 +0000

    fixed bad vareiables

--- 2025/Day 05/day5puzzle2.m
+++ 2025/Day 05/day5puzzle2.m
@@ -2,17 +2,17 @@
 data = readlines("input.txt");
 database = data(1:find(data == "")-1).split("-").double();
 
-ranges = db(1,:);
-for idx = 2:height(db)
-    lowVal = db(idx,1); highVal = db(idx,2);
+ranges = database(1,:);
+for idx = 2:height(database)
+    lowVal = database(idx,1); highVal = database(idx,2);
     overlap = lowVal <= ranges(:,2) & highVal >= ranges(:,1);
     if nnz(overlap)
-        pool = [db(idx,:);ranges(overlap,:)];
+        pool = [database(idx,:);ranges(overlap,:)];
         newR = [min(pool,[],'all'),max(pool,[],'all')];
         ranges(overlap,:) = [];
         ranges = [ranges; newR];
     else
-        ranges = [ranges; db(idx,:)];
+        ranges = [ranges; database(idx,:)];
     end
 end
 format longg
