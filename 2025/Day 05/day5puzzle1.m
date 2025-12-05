76d6bd9a80e1782e25e54e08c815796ec0e99599
Daniel Breslan <daniel@breslan.co.uk>
2025-12-05 07:56:54 +0000

    Day 5 Done.

--- 2025/Day 05/day5puzzle1.m
+++ 2025/Day 05/day5puzzle1.m
@@ -1,5 +1,6 @@
 %% day5puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
-
-day5puzzle1result = 0;
+data = readlines("input.txt");
+splitIdx = find(data == "");
+database = data(1:splitIdx-1).split("-").double();
+ingredients = reshape(data(splitIdx+1:end).double(),1,1,[]);
+day5puzzle1result = sum(squeeze(any(ingredients >= database(:,1) & ingredients <= database(:,2))))
--- 2025/Day 05/day5puzzle2.m
+++ 2025/Day 05/day5puzzle2.m
@@ -1,5 +1,19 @@
 %% day5puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = readlines("input.txt");
+database = data(1:find(data == "")-1).split("-").double();
 
-
-day5puzzle2result = 0;
+ranges = db(1,:);
+for idx = 2:height(db)
+    lowVal = db(idx,1); highVal = db(idx,2);
+    overlap = lowVal <= ranges(:,2) & highVal >= ranges(:,1);
+    if nnz(overlap)
+        pool = [db(idx,:);ranges(overlap,:)];
+        newR = [min(pool,[],'all'),max(pool,[],'all')];
+        ranges(overlap,:) = [];
+        ranges = [ranges; newR];
+    else
+        ranges = [ranges; db(idx,:)];
+    end
+end
+format longg
+day5puzzle2result = sum(ranges(:,2) - ranges(:,1) + 1)
