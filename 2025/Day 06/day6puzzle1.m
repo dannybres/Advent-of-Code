4f1ac24b59d2f3560efad0b1eba6cd21f70f8f19
Daniel Breslan <daniel@breslan.co.uk>
2025-12-06 08:28:13 +0000

    Day 6 done. :)

--- 2025/Day 06/day6puzzle1.m
+++ 2025/Day 06/day6puzzle1.m
@@ -1,5 +1,14 @@
 %% day6puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = strtrim(readlines("input.txt").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ")).split(" ");
 
+numbers = data(1:end-1,:).double();
+operators = data(end,:);
+
+functions = repmat({@sum},size(operators));
+functions(operators == "*") = {@prod};
 
 day6puzzle1result = 0;
+for idx = 1:numel(functions)
+    day6puzzle1result = day6puzzle1result + functions{idx}(numbers(:,idx));
+end
+day6puzzle1result
--- 2025/Day 06/day6puzzle2.m
+++ 2025/Day 06/day6puzzle2.m
@@ -1,5 +1,22 @@
 %% day6puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = char(readlines("input.txt"))';
 
+number = [str2double(strtrim(string(data(:,1:end-1))));nan];
+operators = string(data(:,end));
 
-day6puzzle2result = 0;
+day6puzzle1result = 0;
+for idx = 1:numel(number)
+    if isnan(number(idx))
+        day6puzzle1result = day6puzzle1result + soFar;
+        continue
+    end
+    if operators(idx) == "+"
+        f = @sum;
+        soFar = 0;
+    elseif operators(idx) == "*"
+        f = @prod;
+        soFar = 1;
+    end
+    soFar = f([soFar,number(idx)]);
+end
+day6puzzle1result
