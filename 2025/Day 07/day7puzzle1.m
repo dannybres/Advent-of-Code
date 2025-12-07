3e8425d2684e90895952a11f0dc68c05d7fa9654
Daniel Breslan <daniel@breslan.co.uk>
2025-12-07 11:46:54 +0000

    Day 7 done.

--- 2025/Day 07/day7puzzle1.m
+++ 2025/Day 07/day7puzzle1.m
@@ -1,5 +1,23 @@
 %% day7puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+clc
+data = char(readlines("input.txt"));
 
-
+water = data == 'S';
+splitters = data == '^';
 day7puzzle1result = 0;
+while true
+    waterDepth = find(any(water,2),1,'last');
+    nextDepth = waterDepth + 1;
+    if nextDepth > height(water)
+        break
+    end
+    water(nextDepth,:) = water(waterDepth,:);
+
+    if (any(water(nextDepth,:) & splitters(nextDepth,:)))
+        splitterColisions = find(water(nextDepth,:) & splitters(nextDepth,:))';
+        water(nextDepth,splitterColisions) = false;
+        water(nextDepth, splitterColisions + [-1 1]) = true;
+        day7puzzle1result = day7puzzle1result + numel(splitterColisions);
+    end
+end
+day7puzzle1result
--- 2025/Day 07/day7puzzle2.m
+++ 2025/Day 07/day7puzzle2.m
@@ -1,5 +1,23 @@
 %% day7puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = char(readlines("input.txt"));
+clc
 
+water = double(data == 'S');
+splitters = data == '^';
+while true
+% for a = 1:5
+    waterDepth = find(any(water,2),1,'last');
+    nextDepth = waterDepth + 1;
+    if nextDepth > height(water)
+        break
+    end
+    water(nextDepth,:) = water(waterDepth,:);
 
-day7puzzle2result = 0;
+    for splitterColisionCol = find(water(nextDepth,:) & splitters(nextDepth,:))
+        water(nextDepth, splitterColisionCol + [-1 1]) = ...
+            water(nextDepth, splitterColisionCol + [-1 1]) + water(waterDepth,splitterColisionCol);
+        water(nextDepth,splitterColisionCol) = 0;
+    end
+end
+format longg
+day7puzzle2result = sum(water(end,:))
