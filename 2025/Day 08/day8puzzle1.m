a8be020efc74159bba508d8caea54913d8e05c89
Daniel Breslan <daniel@breslan.co.uk>
2025-12-08 10:47:37 +0000

    Day 8 done.

--- 2025/Day 08/day8puzzle1.m
+++ 2025/Day 08/day8puzzle1.m
@@ -1,5 +1,34 @@
 %% day8puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(",").double;
 
+distances = triu(sqrt((data(:,1) - data(:,1)').^2 + (data(:,2) - data(:,2)').^2 + (data(:,3) - data(:,3)').^2));
+distances(distances == 0) = nan;
+[~, linearIdx] = sort(distances(:));
+[rowIdx, colIdx] = ind2sub(size(distances), linearIdx);
+links = {[rowIdx(1),colIdx(1)]};
+pairsOfJunctionBoxes = 1000;
+for idx = 1:pairsOfJunctionBoxes
+    jb1 = rowIdx(idx); jb2 = colIdx(idx);
+    includedIn = isItIn(links,[jb1,jb2]);
+    switch nnz(~isnan(includedIn))
+        case 0 % in none, add it
+            links = [links;{[jb1,jb2]}];
+        case 1 % in one, append it
+            mergeWith = includedIn(~isnan(includedIn));
+            links{mergeWith} = unique([links{mergeWith},jb1,jb2]);
+        case 2 % in two, merge them
+            if diff(includedIn) == 0, continue, end
+            links{includedIn(1)} = unique([[links{includedIn}],jb1,jb2]);
+            links(includedIn(2)) = [];
+    end
+end
 
-day8puzzle1result = 0;
+lengths = sort(cellfun(@(x) numel(x),links) - 1,'descend')+1;
+day8puzzle1result = prod(lengths(1:3))
+
+function includedIn = isItIn(cells,elements)
+includedIn = nan(1,2);
+for idx = 1:numel(cells)
+    includedIn(any(elements == cells{idx}')) = idx;
+end
+end
--- 2025/Day 08/day8puzzle2.m
+++ 2025/Day 08/day8puzzle2.m
@@ -1,5 +1,35 @@
 %% day8puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = readlines("input.txt").split(",").double;
 
+distances = triu(sqrt((data(:,1) - data(:,1)').^2 + (data(:,2) - data(:,2)').^2 + (data(:,3) - data(:,3)').^2));
+distances(distances == 0) = nan;
+[~, linearIdx] = sort(distances(:));
+[rowIdx, colIdx] = ind2sub(size(distances), linearIdx);
+links = {[rowIdx(1),colIdx(1)]};
+for idx = 2:numel(colIdx)
+    jb1 = rowIdx(idx); jb2 = colIdx(idx);
+    includedIn = isItIn(links,[jb1,jb2]);
+    switch nnz(~isnan(includedIn))
+        case 0 % in none, add it
+            links = [links;{[jb1,jb2]}];
+        case 1 % in one, append it
+            mergeWith = includedIn(~isnan(includedIn));
+            links{mergeWith} = unique([links{mergeWith},jb1,jb2]);
+        case 2 % in two, merge them
+            if diff(includedIn) == 0, continue, end
+            links{includedIn(1)} = unique([[links{includedIn}],jb1,jb2]);
+            links(includedIn(2)) = [];
+    end
+    if numel(links{1}) == height(data)
+        break
+    end
+end
+format longg
+day8puzzle2result = data(jb1,1) * data(jb2,1)
 
-day8puzzle2result = 0;
+function includedIn = isItIn(cells,elements)
+includedIn = nan(1,2);
+for idx = 1:numel(cells)
+    includedIn(any(elements == cells{idx}')) = idx;
+end
+end
