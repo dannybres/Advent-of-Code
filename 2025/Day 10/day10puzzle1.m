fc6d505a0edb172f5c04f8d67e66bfdbabf396ed
Daniel Breslan <daniel@breslan.co.uk>
2025-12-10 11:05:25 +0000

    Day 10 done

--- 2025/Day 10/day10puzzle1.m
+++ 2025/Day 10/day10puzzle1.m
@@ -1,5 +1,29 @@
 %% day10puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
-
+data = readlines("input.txt");
 day10puzzle1result = 0;
+
+for dataIdx = 1:numel(data)
+    machineInfo = data(dataIdx).split(" ");
+
+    target = char(machineInfo(1).extractAfter(1)...
+        .extractBefore(strlength(machineInfo(1))-1)) == '#';
+
+    buttonOutputs = false(numel(machineInfo) - 2, numel(target));
+    for idx = 2:numel(machineInfo) -1
+        buttonOutputs(idx-1,machineInfo(idx)...
+            .extract(digitsPattern).double + 1) = true;
+    end
+    
+    pressOptions = dec2bin(1:(2^height(buttonOutputs)-1)) == '1';
+    [~,so] = sort(sum(pressOptions,2));
+    pressOptions = pressOptions(so,:);
+
+    for idx = 1:height(pressOptions)
+        if all(mod(sum(pressOptions(idx,:)' .* buttonOutputs),2) == target)
+            break
+        end
+    end
+
+    day10puzzle1result = day10puzzle1result + nnz(pressOptions(idx,:));
+end
+day10puzzle1result
--- 2025/Day 10/day10puzzle2.m
+++ 2025/Day 10/day10puzzle2.m
@@ -1,5 +1,24 @@
 %% day10puzzle2 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
-
-
+clc
+data = readlines("input.txt");
 day10puzzle2result = 0;
+a = 0;
+for dataIdx = 1:numel(data)
+    machineInfo = data(dataIdx).split(" ");
+
+    target = machineInfo(end).extract(digitsPattern).double';
+    numButtons = numel(machineInfo) - 2;
+
+    buttonOutputs = false(numButtons, numel(target));
+    for idx = 2:numel(machineInfo) -1
+        buttonOutputs(idx-1,machineInfo(idx)...
+            .extract(digitsPattern).double + 1) = true;
+    end
+    opts = optimoptions('intlinprog', 'Display', 'off');
+    presses = sum(intlinprog(ones(numButtons,1), 1:numButtons, [], [], ...
+        double(buttonOutputs)', target', zeros(numButtons,1),[],opts));
+    
+    day10puzzle2result = day10puzzle2result + presses;
+end
+
+day10puzzle2result
