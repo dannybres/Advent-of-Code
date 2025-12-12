09dff75384aac6c77bdfadd7a5f96c7a17c3daa1
Daniel Breslan <daniel@breslan.co.uk>
2025-12-12 11:18:12 +0000

    Day 12 done.

--- 2025/Day 12/day12puzzle1.m
+++ 2025/Day 12/day12puzzle1.m
@@ -1,5 +1,17 @@
 %% day12puzzle1 - Daniel Breslan - Advent Of Code 2025
-data = readlines("inputDemo.txt")
+data = readlines("input.txt");
+split = find(data.contains("x"),1)-1;
 
+regionsData = data(split+1:end).extract(digitsPattern).double;
 
-day12puzzle1result = 0;
+% do the 3x3 squares fit?
+regionAreas = prod(regionsData(:,1:2),2);
+numPieces = sum(regionsData(:,3:end),2);
+day12puzzle1result = nnz(regionAreas >= numPieces * 9)
+
+% do the number of hashes in the presents fit?
+presentData = data(1:split-1);
+presentData(presentData == "") = ",";
+presentData = presentData.join("").split(",").count("#");
+numHash = sum(regionsData(:,3:end) .* repmat(presentData',height(regionsData),1),2);
+day12puzzle1result = nnz(regionAreas >= numHash)
