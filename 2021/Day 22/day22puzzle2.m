4abce93794ff7fd55d49e4cdb76e29129ad5e42f
Daniel Breslan <daniel@breslan.co.uk>
2025-12-16 10:16:18 +0000

    Day 22 done with negative cubiods, thanks reddit! :)

--- 2021/Day 22/day22puzzle2.m
+++ 2021/Day 22/day22puzzle2.m
@@ -1,24 +1,58 @@
-data = readlines("inputDemo.txt");
-% minR = -50;
-% maxR = 50;
+data = readlines("input.txt");
+tic
+state = (data.extractBefore(" ") == "on") * 2 - 1;
+nums = data.extractAfter(" ").split(",").extractAfter(2).split("..").double();
 
-data = data.split(" ");
+x = squeeze(nums(:,1,:)); y = squeeze(nums(:,2,:)); z = squeeze(nums(:,3,:));
 
-d = data(:,2);
-ins = data(:,1);
-[mat,tok,ext] = regexp(d,"x=(-*\d+)..(-*\d+),y=(-*\d+)..(-*\d+),z=(-*\d+)..(-*\d+)", 'match', ...
-               'tokens', 'tokenExtents');
-t = [tok{:}];
-t = t';
-t = reshape([t{:}],6,[]).double';
+cubes = nan(1e5,7); % xmin xmax ymin ymax zmin zmax sign
+cubeIdx = 1;
+for idx = 1:height(data)
+    nextCube = [x(idx,:), y(idx,:), z(idx,:), state(idx)];
+    allIntersects = find(intersects(nextCube,cubes(1:cubeIdx-1,:)));
+    intersections = nan(numel(allIntersects),7);
+    for iIdx = 1:numel(allIntersects)
+        intersections(iIdx,:) = intersection(nextCube,cubes(allIntersects(iIdx),:));
+    end
+    cubes(cubeIdx:cubeIdx + numel(allIntersects) - 1,:) = intersections;
+    cubeIdx = cubeIdx + numel(allIntersects);
+    if nextCube(7) == 1
+        cubes(cubeIdx,:) = nextCube;
+        cubeIdx = cubeIdx + 1;
+    end
+end
+day22puzzle2result = sum(...
+    (cubes(1:cubeIdx-1,2) - cubes(1:cubeIdx-1,1) + 1) .* ...
+    (cubes(1:cubeIdx-1,4) - cubes(1:cubeIdx-1,3) + 1) .* ...
+    (cubes(1:cubeIdx-1,6) - cubes(1:cubeIdx-1,5) + 1) .* ...
+    cubes(1:cubeIdx-1,7))
+toc
 
-% ins = ins(all([t(:,1:2:end)<maxR t(:,2:2:end) > minR],2));
-% t = t(all([t(:,1:2:end)<maxR t(:,2:2:end) > minR],2),:);
+function bool = intersects(A,B)
+    if isempty(B)
+        bool = false;
+        return
+    end
+    bool = A(1) <= B(:,2) & A(2) >= B(:,1) & ...
+           A(3) <= B(:,4) & A(4) >= B(:,3) & ...
+           A(5) <= B(:,6) & A(6) >= B(:,5);
+end
 
-t = t - repelem(min(t(:,1:2:end)),1,2) + 1;
+function intersectCube = intersection(newCube,existingCube)
+    minX = max([newCube(1), existingCube(1)]);
+    maxX = min([newCube(2), existingCube(2)]);
 
-foo = zeros(max(t(:,2:2:end)));
-f = "foo(%d:%d,%d:%d,%d:%d) = ";
-eval(join(compose(f,t) + double(ins == "on"),";")+";");
+    minY = max([newCube(3), existingCube(3)]);
+    maxY = min([newCube(4), existingCube(4)]);
 
-sum(foo,'all')
+    minZ = max([newCube(5), existingCube(5)]);
+    maxZ = min([newCube(6), existingCube(6)]);
+
+    if newCube(7) == existingCube(7)
+        sign = -newCube(7);
+    else
+        sign = newCube(7);
+    end
+
+    intersectCube = [minX, maxX, minY, maxY, minZ, maxZ, sign];
+end
